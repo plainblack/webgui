773e7f76d1e0e7ee067e027bac9b2e08f692c650
JT Smith <jt@plainblack.com>
2002-05-04 00:32:31 +0000

    Added this macro per user request.

---
+++ lib/WebGUI/Macro/Extras.pm
@@ -0,0 +1,25 @@
+package WebGUI::Macro::Extras;
+
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2002 Plain Black Software.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+use strict;
+use WebGUI::Session;
+
+#-------------------------------------------------------------------
+sub process {
+	my ($output);
+	$output = $_[0];
+        $output =~ s/\^Extras\;/$session{setting}{lib}\//g;
+	return $output;
+}
+
+1;
+
