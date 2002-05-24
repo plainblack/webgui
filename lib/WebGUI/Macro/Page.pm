87ffd9af00e7ef09a81c04936a4c51669ead5498
JT Smith <jt@plainblack.com>
2002-05-24 16:57:29 +0000

    Added ^Page(); macro at Support Center user request.

---
+++ lib/WebGUI/Macro/Page.pm
@@ -0,0 +1,35 @@
+package WebGUI::Macro::Page;
+
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2002 Plain Black Software.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+use strict;
+use WebGUI::Macro;
+use WebGUI::Session;
+
+#-------------------------------------------------------------------
+sub _replacement {
+	my (@param, $temp);
+        @param = WebGUI::Macro::getParams($_[0]);
+	$temp = $session{page}{$param[0]};
+	return $temp;
+}
+
+#-------------------------------------------------------------------
+sub process {
+	my ($output, $temp);
+	$output = $_[0];
+        $output =~ s/\^Page\((.*?)\)\;/_replacement($1)/ge;
+	return $output;
+}
+
+1;
+
+
