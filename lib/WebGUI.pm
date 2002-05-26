d4d7cd8db168be53eb69b62f3890efd3d1a3bf6a
JT Smith <jt@plainblack.com>
2002-05-26 16:45:11 +0000

    Set up 3.9.0.

--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -1,5 +1,5 @@
 package WebGUI;
-our $VERSION = "3.8.0";
+our $VERSION = "3.9.0";
 
 #-------------------------------------------------------------------
 # WebGUI is Copyright 2001-2002 Plain Black Software.
