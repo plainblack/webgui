3b64444f57415fb903750c1c3d073ff700b05e3c
JT Smith <jt@plainblack.com>
2002-06-09 00:29:13 +0000

    Preparing for 4.0.1 bugfix cycle.

--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -1,5 +1,5 @@
 package WebGUI;
-our $VERSION = "4.0.0";
+our $VERSION = "4.0.1";
 
 #-------------------------------------------------------------------
 # WebGUI is Copyright 2001-2002 Plain Black Software.
