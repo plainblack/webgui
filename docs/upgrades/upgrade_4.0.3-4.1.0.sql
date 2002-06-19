428dd45110c20cbafe013beb86fbaa75cd9103df
JT Smith <jt@plainblack.com>
2002-06-19 00:45:30 +0000

    Adding karma.

--- docs/upgrades/upgrade_4.0.3-4.1.0.sql
+++ docs/upgrades/upgrade_4.0.3-4.1.0.sql
@@ -1,2 +1,10 @@
 insert into webguiVersion values ('4.1.0','upgrade',unix_timestamp());
+alter table users add column karma int not null default 0;
+alter table groups add column karmaThreshold int not null default 99999999999;
+create table karmaLog (
+ userId int not null,
+ amount int not null default 1,
+ source varchar(255),
+ description text
+);
 
