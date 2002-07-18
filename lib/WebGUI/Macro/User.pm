692eef8efa9f57ff1801432a6ce74f95b8c66040
JT Smith <jt@plainblack.com>
2002-07-18 03:17:27 +0000

    Added User macro.

---
+++ lib/WebGUI/Macro/User.pm
@@ -0,0 +1,35 @@
+package WebGUI::Macro::User;
+
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2002 Plain Black LLC.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+use strict;
+use WebGUI::Macro;
+use WebGUI::Session;
+
+#-------------------------------------------------------------------
+sub _replacement {
+	my (@param, $temp);
+        @param = WebGUI::Macro::getParams($_[0]);
+	$temp = $session{user}{$param[0]};
+	return $temp;
+}
+
+#-------------------------------------------------------------------
+sub process {
+	my ($output, $temp);
+	$output = $_[0];
+        $output =~ s/\^User\((.*?)\)\;/_replacement($1)/ge;
+	return $output;
+}
+
+1;
+
+
