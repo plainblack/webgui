5875d4d6462efd35e3ac09dd26c019490db01903
JT Smith <jt@plainblack.com>
2002-08-14 00:58:02 +0000

    Added LoginToggle macro

--- docs/upgrades/upgrade_4.5.0-4.6.0.sql
+++ docs/upgrades/upgrade_4.5.0-4.6.0.sql
@@ -1,2 +1,11 @@
 insert into webguiVersion values ('4.6.0','upgrade',unix_timestamp());
+insert into international values (716,'WebGUI',1,'Login');
+insert into international values (717,'WebGUI',1,'Logout');
+
+
+
+
+
+
+
 
