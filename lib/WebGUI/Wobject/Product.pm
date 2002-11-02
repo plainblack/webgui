9406e3684ff7e5f5801aef28d5cb217f5125c324
JT Smith <jt@plainblack.com>
2002-11-02 17:21:48 +0000

    Wasn't displaying the title.

--- lib/WebGUI/Wobject/Product.pm
+++ lib/WebGUI/Wobject/Product.pm
@@ -654,7 +654,7 @@ sub www_view {
         }
         $sth->finish;
 	$var{relatedproduct_loop} = \@relatedloop;
-        return $_[0]->processMacros($_[0]->processTemplate($_[0]->get("templateId"),\%var));
+        return $_[0]->processMacros($output.$_[0]->processTemplate($_[0]->get("templateId"),\%var));
 }
 
 
