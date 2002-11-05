4c0ee71ab5abd950f83f51deff5fff4b10748344
JT Smith <jt@plainblack.com>
2002-11-05 03:19:28 +0000

    Preparing for the 4.8.0 dev cycle

---
+++ docs/upgrades/upgrade_4.7.0-4.8.0.sql
@@ -0,0 +1,8 @@
+insert into webguiVersion values ('4.8.0','upgrade',unix_timestamp());
+
+
+
+
+
+
+
--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -1,5 +1,5 @@
 package WebGUI;
-our $VERSION = "4.7.0";
+our $VERSION = "4.8.0";
 
 #-------------------------------------------------------------------
 # WebGUI is Copyright 2001-2002 Plain Black LLC.
