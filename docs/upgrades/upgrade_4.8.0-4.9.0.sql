04c431306e3c9d2b55cb7974512c4f100de77f07
JT Smith <jt@plainblack.com>
2002-11-23 02:58:43 +0000

    fixing the new pluggable auth converter

--- docs/upgrades/upgrade_4.8.0-4.9.0.sql
+++ docs/upgrades/upgrade_4.8.0-4.9.0.sql
@@ -1,5 +1,11 @@
 insert into webguiVersion values ('4.9.0','upgrade',unix_timestamp());
-
+create table authentication (userId int(11) not null, authMethod varchar(30) not null, fieldName varchar(128) not null, fieldData text, primary key (userId, authMethod, fieldName));
+insert into authentication select userId,'LDAP','ldapUrl',ldapURL from users;
+insert into authentication select userId,'LDAP','connectDN',connectDN from users;
+insert into authentication select userId,'WebGUI','identifier',identifier from users;
+alter table users drop column identifier;
+alter table users drop column ldapURL;
+alter table users drop column connectDN;
 
 
 
