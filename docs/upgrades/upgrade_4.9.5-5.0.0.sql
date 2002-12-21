d5a307efaedf9ff6801281f2b373e19930664517
JT Smith <jt@plainblack.com>
2002-12-21 17:22:12 +0000

    Starting 5.0.0 bugfix cycle.

---
+++ docs/upgrades/upgrade_4.9.5-5.0.0.sql
@@ -0,0 +1,4 @@
+insert into webguiVersion values ('5.0.0','upgrade',unix_timestamp());
+insert into international (internationalId,languageId,namespace,message,lastUpdated) values (856,3,'WebGUI','Er zijn op dit moment geen account eigenschappen die u mag aanpassen.', 1040350000);
+
+
--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -1,5 +1,5 @@
 package WebGUI;
-our $VERSION = "4.9.5";
+our $VERSION = "5.0.0";
 
 #-------------------------------------------------------------------
 # WebGUI is Copyright 2001-2002 Plain Black LLC.
