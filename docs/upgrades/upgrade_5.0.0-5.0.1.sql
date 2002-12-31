25cea217ab353a1adf028c5287f26d123074e130
JT Smith <jt@plainblack.com>
2002-12-31 18:50:53 +0000

    preparing for the 5.0.1 bugfix cycle.

---
+++ docs/upgrades/upgrade_5.0.0-5.0.1.sql
@@ -0,0 +1,2 @@
+insert into webguiVersion values ('5.0.1','upgrade',unix_timestamp());
+
--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -1,5 +1,5 @@
 package WebGUI;
-our $VERSION = "5.0.0";
+our $VERSION = "5.0.1";
 
 #-------------------------------------------------------------------
 # WebGUI is Copyright 2001-2002 Plain Black LLC.
