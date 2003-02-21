4e995570b2c15da66c8028c2fe2637e689f384ec
JT Smith <jt@plainblack.com>
2003-02-21 03:59:10 +0000

    removed info that was mistakenly added to this file and put it in the proper file.

--- docs/upgrades/upgrade_5.1.1-5.1.2.sql
+++ docs/upgrades/upgrade_5.1.1-5.1.2.sql
@@ -104,13 +104,3 @@ insert into international (internationalId,languageId,namespace,message,lastUpda
 delete from international where languageId=2 and namespace='WebGUI' and internationalId=887;
 insert into international (internationalId,languageId,namespace,message,lastUpdated) values (887,2,'WebGUI','Midas (IE 6+ or Mozilla 1.3+)', 1045210016);
 
-alter table MailForm_field add column validation varchar(255) default NULL;
-alter table MailForm_field add column subtext mediumtext default NULL;
-alter table MailForm_field add column rows int(11) default NULL;
-insert into international (internationalId,languageId,namespace,message,lastUpdated) values (27,1,'MailForm','Height', 1045210016);
-insert into international (internationalId,languageId,namespace,message,lastUpdated) values (28,1,'MailForm','Optional for TextArea', 1045210016);
-insert into international (internationalId,languageId,namespace,message,lastUpdated) values (29,1,'MailForm','is a required field.', 1045210016);
-insert into international (internationalId,languageId,namespace,message,lastUpdated) values (30,1,'MailForm','has to be a number.', 1045210016);
-insert into international (internationalId,languageId,namespace,message,lastUpdated) values (31,1,'MailForm','is not filled in correctly.', 1045210016);
-insert into international (internationalId,languageId,namespace,message,lastUpdated) values (32,1,'MailForm','is not a valid email address.', 1045210016);
-update international set message="Possible Values" where namespace = "MailForm" and internationalId = 24 and languageId=1;
