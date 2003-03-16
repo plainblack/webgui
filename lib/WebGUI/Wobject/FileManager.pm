1707f8cd057a650ada69142ed597810cb3d9e46b
JT Smith <jt@plainblack.com>
2003-03-16 00:29:29 +0000

    fixed a syntax error

--- lib/WebGUI/Wobject/FileManager.pm
+++ lib/WebGUI/Wobject/FileManager.pm
@@ -165,7 +165,7 @@ sub www_edit {
         }
 	return $_[0]->SUPER::www_edit(
 		-properties=>$properties->printRowsOnly,
-		-layout=>$layout->printRowsOnly
+		-layout=>$layout->printRowsOnly,
 		-headingId=>9,
 		-helpId=>1
 		);
