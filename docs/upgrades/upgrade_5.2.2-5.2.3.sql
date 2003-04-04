b236fa53c0c6d8587117abba5d8aaecc6a881147
JT Smith <jt@plainblack.com>
2003-04-04 05:13:52 +0000

    fixed a bug in item popup template

--- docs/upgrades/upgrade_5.2.2-5.2.3.sql
+++ docs/upgrades/upgrade_5.2.2-5.2.3.sql
@@ -1,3 +1,6 @@
 insert into webguiVersion values ('5.2.3','upgrade',unix_timestamp());
+delete from template where templateId=2 and namespace='Item';
+INSERT INTO template VALUES (2,'Item w/pop-up Links','<tmpl_if displaytitle>\r\n   <tmpl_if linkurl>\r\n       <a href=\"<tmpl_var linkurl>\" target=\"_blank\">\r\n    </tmpl_if>\r\n     <span class=\"itemTitle\"><tmpl_var title></span>\r\n   <tmpl_if linkurl>\r\n      </a>\r\n    </tmpl_if>\r\n</tmpl_if>\r\n\r\n<tmpl_if attachment.name>\r\n   <tmpl_if displaytitle> - </tmpl_if>\r\n   <a href=\"<tmpl_var attachment.url>\" target=\"_blank\"><img src=\"<tmpl_var attachment.Icon>\" border=\"0\" alt=\"<tmpl_var attachment.name>\" width=\"16\" height=\"16\" border=\"0\" align=\"middle\" /></a>\r\n</tmpl_if>\r\n\r\n<tmpl_if description>\r\n  - <tmpl_var description>\r\n</tmpl_if>','Item');
+
 
 
