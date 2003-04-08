87a06e2331ee5df6203a333114f04988d6cc9401
Len Kranendonk <len@webgui-svn>
2003-04-08 07:58:30 +0000

    fixed bug 717356

--- lib/WebGUI/Wobject/FAQ.pm
+++ lib/WebGUI/Wobject/FAQ.pm
@@ -101,7 +101,7 @@ sub www_edit {
 	}
 	return $_[0]->SUPER::www_edit(
 		-properties=>$properties->printRowsOnly,
-		-layout=>$layout->printRowsOnly
+		-layout=>$layout->printRowsOnly,
 		-headingId=>8,
 		-helpId=>1
 		);
