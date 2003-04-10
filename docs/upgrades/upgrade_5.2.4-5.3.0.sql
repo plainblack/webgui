1158a0aa69400ce967d9ef88b6db33427f9e30b6
JT Smith <jt@plainblack.com>
2003-04-10 05:44:43 +0000

    preparing for the 5.3.0 dev cycle.

---
+++ docs/upgrades/upgrade_5.2.4-5.3.0.sql
@@ -0,0 +1,3 @@
+insert into webguiVersion values ('5.3.0','upgrade',unix_timestamp());
+
+
--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -1,5 +1,5 @@
 package WebGUI;
-our $VERSION = "5.2.4";
+our $VERSION = "5.3.0";
 
 #-------------------------------------------------------------------
 # WebGUI is Copyright 2001-2003 Plain Black LLC.
