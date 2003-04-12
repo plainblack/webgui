a4b291035cf979b68349a1623cdd7ba54a5f440c
JT Smith <jt@plainblack.com>
2003-04-12 03:29:31 +0000

    allowed for "linkonly" attribute

--- lib/WebGUI/Macro/LoginToggle.pm
+++ lib/WebGUI/Macro/LoginToggle.pm
@@ -21,11 +21,17 @@ sub process {
        my (@param, $temp, $login, $logout);
        @param = WebGUI::Macro::getParams($_[0]);
        if ($session{user}{userId} == 1) {
-               $login = $param[0] || WebGUI::International::get(716);
-               $temp = '<a class="loginToggleLink" href="'.WebGUI::URL::page('op=displayLogin').'">'.$login.'</a>';
+		if ($param[0] eq "linkonly") {
+			return WebGUI::URL::page('op=displayLogin');
+		}
+              	$login = $param[0] || WebGUI::International::get(716);
+               	$temp = '<a class="loginToggleLink" href="'.WebGUI::URL::page('op=displayLogin').'">'.$login.'</a>';
        } else {
-               $logout = $param[1] || WebGUI::International::get(717);
-               $temp = '<a class="loginToggleLink" href="'.WebGUI::URL::page('op=logout').'">'.$logout.'</a>';
+		if ($param[0] eq "linkonly") {
+			return WebGUI::URL::page('op=logout');
+		}
+               	$logout = $param[1] || WebGUI::International::get(717);
+               	$temp = '<a class="loginToggleLink" href="'.WebGUI::URL::page('op=logout').'">'.$logout.'</a>';
        }
        return $temp;
 }
