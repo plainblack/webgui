6949f42a2c6fb01c8808d0c29195b31f5f44b075
JT Smith <jt@plainblack.com>
2003-04-18 17:45:48 +0000

    Added groupAdmin field to groupings table for future expansion.

--- docs/upgrades/upgrade_5.2.5-5.3.0.sql
+++ docs/upgrades/upgrade_5.2.5-5.3.0.sql
@@ -275,5 +275,9 @@ delete from international where languageId=1 and namespace='WebGUI' and internat
 insert into international (internationalId,languageId,namespace,message,lastUpdated) values (831,1,'WebGUI','Collateral Macros', 1050441851);
 alter table international add column context text;
 insert into groups (groupId,groupName,description) values (10,"Translation Managers","Users that can edit language translations for WebGUI.");
+alter table groupings add column groupAdmin int not null default 0;
+
+
+
 
 
