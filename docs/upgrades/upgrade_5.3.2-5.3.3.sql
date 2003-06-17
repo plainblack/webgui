a2486033bf009329101478ceca47639e1828508b
Andy Grundman <agrundma@webgui-cvs>
2003-06-17 19:18:02 +0000

    Corrected message for 'Delete this folder' since it does not move files to the parent

--- docs/upgrades/upgrade_5.3.2-5.3.3.sql
+++ docs/upgrades/upgrade_5.3.2-5.3.3.sql
@@ -1,4 +1,5 @@
 insert into webguiVersion values ('5.3.3','upgrade',unix_timestamp());
 delete from international where internationalId=29 and namespace='DataForm' and languageId=1;
 insert into international (internationalId,namespace,languageId,message,lastUpdated) values (29,'DataForm',1,'is required',1031515049);
-
+delete from international where languageId=1 and namespace='WebGUI' and internationalId=775;
+insert into international (internationalId,languageId,namespace,message,lastUpdated) values (775,1,'WebGUI','Are you certain you wish to delete this folder? It cannot be recovered once deleted.',1055877319);
