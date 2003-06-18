b3ab11467231d3b972a6bf8ee4f1197e020703f3
JT Smith <jt@plainblack.com>
2003-06-18 04:51:48 +0000

    added some new collateral messages

--- docs/upgrades/upgrade_5.3.3-5.4.0.sql
+++ docs/upgrades/upgrade_5.3.3-5.4.0.sql
@@ -1,2 +1,4 @@
 insert into webguiVersion values ('5.4.0','upgrade',unix_timestamp());
+insert into international (internationalId,languageId,namespace,message,lastUpdated) values (979,1,'WebGUI','Are you certain you wish to delete all items in this folder? They cannot be recovered once deleted. Items in sub-folders will not be removed.',1055908341);
+insert into international (internationalId,languageId,namespace,message,lastUpdated) values (980,1,'WebGUI','Empty this folder.',1055908341);
 
