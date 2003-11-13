875c3bd0adbf8c4a9c632d357bd0ccd9cde6f88b
Len Kranendonk <len@webgui-svn>
2003-11-13 12:10:11 +0000

    Initial check-in. / Bugfix 840939

---
+++ docs/upgrades/upgrade_5.5.0-5.5.1.sql
@@ -0,0 +1,3 @@
+insert into webguiVersion values ('5.5.1','upgrade',unix_timestamp());
+INSERT INTO international VALUES (1072,'WebGUI',1,'The email address is already in use. Please use a different email address.',1068703399,'Error message shown when trying to create/update an account with an email address that already exists.');
+
