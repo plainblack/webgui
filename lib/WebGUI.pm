065918f360220e113b95f42501d5eeb24843e547
JT Smith <jt@plainblack.com>
2003-11-22 15:46:05 +0000

    preparing for 5.5.3 dev cycle

--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -1,5 +1,5 @@
 package WebGUI;
-our $VERSION = "5.5.2";
+our $VERSION = "5.5.3";
 
 #-------------------------------------------------------------------
 # WebGUI is Copyright 2001-2003 Plain Black LLC.
