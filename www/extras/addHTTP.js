36a7165d0dd5d155bcd2760ad843e31253a51f88
JT Smith <jt@plainblack.com>
2003-12-29 20:43:30 +0000

    merging 5.5.4 fixes

--- www/extras/addHTTP.js
+++ www/extras/addHTTP.js
@@ -1,8 +1,10 @@
 function addHTTP(element) {
-   if (!element.value.match("http://")) {
-	element.value = "http://"+element.value;
-   }
-   if (!element.value.match(/^http:\/\/.+\..+/)){
-	alert("This is not a valid url. Please check if it is correct.");
-   }
+   	if (element.value != "") {
+   		if (!element.value.match("http://")) {
+			element.value = "http://"+element.value;
+   		}
+		if (!element.value.match(/^http:\/\/.+\..+/)){
+			alert("This is not a valid url. Please check if it is correct.");
+   		}
+	}
 }
