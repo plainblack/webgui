403082ed239629c925a81f468970215c0801b2bd
JT Smith <jt@plainblack.com>
2004-02-29 15:40:11 +0000

    updated copyright year

--- sbin/Hourly/DeleteExpiredSessions.pm
+++ sbin/Hourly/DeleteExpiredSessions.pm
@@ -1,7 +1,7 @@
 package Hourly::DeleteExpiredSessions;
 
 #-------------------------------------------------------------------
-# WebGUI is Copyright 2001-2003 Plain Black LLC.
+# WebGUI is Copyright 2001-2004 Plain Black LLC.
 #-------------------------------------------------------------------
 # Please read the legal notices (docs/legal.txt) and the license
 # (docs/license.txt) that came with this distribution before using
