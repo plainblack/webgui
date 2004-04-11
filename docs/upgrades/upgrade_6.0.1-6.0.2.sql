925de26f972819a2265b5066cf3d4ca1af7ce6cb
JT Smith <jt@plainblack.com>
2004-04-11 21:13:16 +0000

    added german toolbar icons

--- docs/upgrades/upgrade_6.0.1-6.0.2.sql
+++ docs/upgrades/upgrade_6.0.1-6.0.2.sql
@@ -1,2 +1,4 @@
 insert into webguiVersion values ('6.0.2','upgrade',unix_timestamp());
+update language set toolbar='german' where languageId=2;
+alter table language change toolbar toolbar varchar(35) not null default 'metal';
 
