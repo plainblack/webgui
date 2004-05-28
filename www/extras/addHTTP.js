e9cecb762c0d1885c3fd08464193609944b555b6
Leendert Bottelberghs <bottelberghs@webgui-cvs>
2004-05-28 14:21:54 +0000

    Added possibility to enter a WebGUI Macro

--- www/extras/addHTTP.js
+++ www/extras/addHTTP.js
@@ -1,10 +1,12 @@
 function addHTTP(element) {
    	if (element.value != "") {
-   		if (!element.value.match(/^\w+:\/\//)) {
-			element.value = "http://"+element.value;
-   		}
-		if (!element.value.match(/^\w+:\/\/.+\..+/)){
-			alert("That does not look like a proper URL. Please check if it is correct.");
-   		}
+		if (!element.value.match(/^\^(.*);/)){
+			if (!element.value.match(/^\w+:\/\//)) {
+				element.value = "http://"+element.value;
+			}
+			if (!element.value.match(/^\w+:\/\/.+\..+/)){
+				alert("That does not look like a proper URL. Please check if it is correct.");
+			}
+		}
 	}
 }
