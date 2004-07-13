867cc8de540ae6cd44c6c098144f2a5159fca7a4
JT Smith <jt@plainblack.com>
2004-07-13 17:23:53 +0000

    fixing article pagination template variables

--- docs/upgrades/upgrade_6.0.3-6.1.0.pl
+++ docs/upgrades/upgrade_6.0.3-6.1.0.pl
@@ -149,6 +149,17 @@ walk_down(0, {
 	}
 });
 
+
+print "\tFixing pagination template variables.\n" unless ($quiet);
+my $sth = WebGUI::SQL->read("select * from template where namespace in ('Article')");
+while (my $data = $sth->hashRef) {
+        $data->{template} =~ s/pagination\.ispagination\.firstPage/pagination.isFirstPage/ig;
+        $data->{template} =~ s/pagination\.ispagination\.lastPage/pagination.isLastPage/ig;
+        WebGUI::SQL->write("update template set template=".quote($data->{template})." where namespace=".quote($data->{namespace})." and templateId=".quote($data->{templateId}));
+}
+$sth->finish;
+
+
 WebGUI::Session::close();
 
 
