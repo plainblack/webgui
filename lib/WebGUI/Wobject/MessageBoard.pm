125babaf45a0f99e61749558f5b8dcd6813cdc90
Leendert Bottelberghs <bottelberghs@webgui-cvs>
2004-08-26 14:00:01 +0000

    bugfix [ 1016271 ]. all posts go to last forum in MB

--- lib/WebGUI/Wobject/MessageBoard.pm
+++ lib/WebGUI/Wobject/MessageBoard.pm
@@ -261,7 +261,7 @@ sub www_view {
 	my $caller;
 	my $sth = WebGUI::SQL->read("select * from MessageBoard_forums where wobjectId=".quote($_[0]->get("wobjectId"))." order by sequenceNumber");
 	while (my $forumMeta = $sth->hashRef) {
-		my $callback = WebGUI::URL::page("func=view&wid=".$_[0]->get("wobjectId")."&forumId=".$forumMeta->{forumId});
+		my $callback = WebGUI::URL::page("func=view&wid=".$_[0]->get("wobjectId"));
 		if ($session{form}{forumOp}) { 
 			if ($session{form}{forumId} == $forumMeta->{forumId}) {
 				$caller = {
