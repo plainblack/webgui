df7e2657b5e44dd4b71cff03eb1c246843c3b8cc
Colin Kuskie <colink@perldreamer.com>
2004-11-12 05:58:39 +0000

    more typos in AuthLDAP.pm

--- lib/WebGUI/Help/AuthLDAP.pm
+++ lib/WebGUI/Help/AuthLDAP.pm
@@ -6,9 +6,17 @@ our $HELP = {
 		body => 'account-2',
 		related => [
 			{
+				tag => 'ldap authentication anonymous registration template',
+				namespace => 'AuthLDAP'
+			},
+			{
+				tag => 'ldap authentication login template',
+				namespace => 'AuthLDAP'
+			},
+			{
 				tag => 'wobject template',
 				namespace => 'WebGUI'
-			}
+			},
 		]
 	},
 	'ldap authentication login template' => {
@@ -16,15 +24,31 @@ our $HELP = {
 		body => 'login-2',
 		related => [
 			{
+				tag => 'ldap authentication anonymous registration template',
+				namespace => 'AuthLDAP'
+			},
+			{
+				tag => 'ldap authentication display account template',
+				namespace => 'AuthLDAP'
+			},
+			{
 				tag => 'wobject template',
 				namespace => 'WebGUI'
-			}
+			},
 		]
 	},
 	'ldap authentication anonymous registration template' => {
 		title => 'create-1',
 		body => 'create-2',
 		related => [
+			{
+				tag => 'ldap authentication display account template',
+				namespace => 'AuthLDAP'
+			},
+			{
+				tag => 'ldap authentication login template',
+				namespace => 'AuthLDAP'
+			},
 			{
 				tag => 'wobject template',
 				namespace => 'WebGUI'
