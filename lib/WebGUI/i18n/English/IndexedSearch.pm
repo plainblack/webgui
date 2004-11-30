c167fe2767f6c1fd5f13b4893ec6681eb56aff32
Colin Kuskie <colink@perldreamer.com>
2004-11-30 00:17:41 +0000

    spelling and HTML

--- lib/WebGUI/i18n/English/IndexedSearch.pm
+++ lib/WebGUI/i18n/English/IndexedSearch.pm
@@ -69,23 +69,29 @@ our $I18N = {
 	'27' => {
 		message => q|
 <P>The Search adds advanced search capabilities to your WebGUI site. </P>
-<P><STRONG>Index to use<BR></STRONG>The Search uses an index to retrieve it's 
+<P><STRONG>Index to use</STRONG><BR>
+The Search uses an index to retrieve it's 
 results from. Indexes are created with the scheduler. You can create more then one index. Choose here which index to use.</P>
-<P><STRONG>Search through<BR></STRONG>By default all pages are searched. You can 
+<P><STRONG>Search through</STRONG><BR>
+By default all pages are searched. You can 
 limit the search to certain page roots. Multiple choices are allowed.</P>
-<P><STRONG>Only results created by<BR></STRONG>You can limit the results to 
+<P><STRONG>Only results created by</STRONG><BR>
+You can limit the results to 
 items created by certain users. By default items from any user are returned.</P>
-<P><STRONG>Only results in namespace<BR></STRONG>By default all namespaces are 
+<P><STRONG>Only results in namespace</STRONG><BR>
+By default all namespaces are 
 searched. You can limit the search to certain namespaces. An example of usage is 
 to search only in products.</P>
-<P><STRONG>Only results in language<BR></STRONG>If you have a multi-lingual 
+<P><STRONG>Only results in language</STRONG><BR>
+If you have a multi-lingual 
 site, you can use this option to limit the search results to a certain 
 language.</P>
-<P><STRONG>Only results of type<BR></STRONG>You can limit the search to certain 
+<P><STRONG>Only results of type</STRONG><BR>
+You can limit the search to certain 
 types of content.</P>
 <BLOCKQUOTE dir=ltr style="MARGIN-RIGHT: 0px">
-<P align=left><EM>Discussion:</EM> Messages on the forums,&nbsp;discussions on 
-articles or&nbsp;USS.<BR><EM>Help:</EM> Content in the online WebGUI help 
+<P align=left><EM>Discussion:</EM> Messages on the forums, discussions on 
+articles or USS.<BR><EM>Help:</EM> Content in the online WebGUI help 
 system<BR><EM>Page:</EM><STRONG> </STRONG>Page title and 
 synopsis<BR><EM>Profile:</EM> User Profiles<BR><EM>Wobject: </EM>Wobject Title 
 and Description<BR><EM>Wobject details: </EM>All other wobject data. For example 
@@ -102,8 +108,8 @@ no preview.</P>
 <P dir=ltr align=left><STRONG>Highlight results ?<BR></STRONG>If you want to 
 highlight the search results in the preview you'll want to check this box.</P>
 <P dir=ltr align=left><STRONG>Highlight color n<BR></STRONG>The colors that are 
-used to highlight the corresponding words in the query.&nbsp;</P>|,
-		lastUpdated => 1090580644
+used to highlight the corresponding words in the query.</P>|,
+		lastUpdated => 1101773588
 	},
 
 	'25' => {
@@ -119,56 +125,59 @@ used to highlight the corresponding words in the query.&nbsp;</P>|,
 	'28' => {
 		message => q|
 <P>This is the list of template variables available for 
-search&nbsp;templates:</P>
-<P><STRONG>query<BR></STRONG>Contains the value of the <EM>query</EM> form 
+search templates:</P>
+<P><STRONG>query</STRONG><BR>
+Contains the value of the <EM>query</EM> form 
 variable. <BR>The <EM>allWords</EM>, <EM>atLeastOne</EM>, <EM>exactPhrase</EM> 
 and <EM>without</EM> values are appended to this variable.</P>
-<P><STRONG>queryHighlighted<BR></STRONG>Same as <STRONG>query</STRONG> but 
-highlighted.</P>
-<P><STRONG>allWords<BR></STRONG>Contains the value of the <EM>allWords</EM> form 
-variable.</P>
-<P><STRONG>atLeastOne<BR></STRONG>Contains the value of the <EM>atLeastOne</EM> 
-form variable.</P>
-<P><STRONG>exactPhrase<BR></STRONG>Contains the value of the 
-<EM>exactPhrase</EM> form variable.</P>
-<P><STRONG>without<BR></STRONG>Contains the value of the <EM>without </EM>form 
-variable.</P>
-<P><STRONG>duration<BR></STRONG>The duration of the search process in seconds. 
-</P>
-<P><STRONG>numberOfResults<BR></STRONG>The number of results. </P>
-<P><STRONG>startNr<BR></STRONG>The number of the first search result on the 
-page.</P>
-<P><STRONG>endNr<BR></STRONG>The number of the last search result on the 
-page.</P>
-<P><STRONG>submit<BR></STRONG>A form button with the word "Search" printed on 
-it. </P>
-<P><STRONG>wid</STRONG><BR>The wobject Id of this wobject.</P>
-<P><STRONG>resultsLoop</STRONG><BR>A loop containing the search results. Inside 
-the loop the following template variables are available:</P>
+<P><STRONG>queryHighlighted</STRONG><BR>
+Same as <STRONG>query</STRONG> but highlighted.</P>
+<P><STRONG>allWords</STRONG><BR>
+Contains the value of the <EM>allWords</EM> form variable.</P>
+<P><STRONG>atLeastOne</STRONG><BR>
+Contains the value of the <EM>atLeastOne</EM> form variable.</P>
+<P><STRONG>exactPhrase</STRONG><BR>
+Contains the value of the <EM>exactPhrase</EM> form variable.</P>
+<P><STRONG>without</STRONG><BR>
+Contains the value of the <EM>without</EM> form variable.</P>
+<P><STRONG>duration</STRONG><BR>
+The duration of the search process in seconds.</P>
+<P><STRONG>numberOfResults</STRONG><BR>
+The number of results.</P>
+<P><STRONG>startNr</STRONG><BR>
+The number of the first search result on the page.</P>
+<P><STRONG>endNr</STRONG><BR>
+The number of the last search result on the page.</P>
+<P><STRONG>submit</STRONG><BR>
+A form button with the word "Search" printed on it.</P>
+<P><STRONG>wid</STRONG><BR>
+The wobject Id of this wobject.</P>
+<P><STRONG>resultsLoop</STRONG><BR>
+A loop containing the search results. Inside the loop the following template variables are available:</P>
 <BLOCKQUOTE dir=ltr style="MARGIN-RIGHT: 0px">
-<P><STRONG>username<BR></STRONG>The username of the person that created this 
-search result.</P>
-<P><STRONG>ownerId<BR></STRONG>The Id of the person that created this search 
-result.</P>
-<P><STRONG>userProfile<BR></STRONG>An url to the profile of the creator of this 
-search result.</P>
-<P><STRONG>header<BR></STRONG>The title of the search result. (This can be the 
+<P><STRONG>username</STRONG><BR>
+The username of the person that created this search result.</P>
+<P><STRONG>ownerId</STRONG><BR>
+The Id of the person that created this search result.</P>
+<P><STRONG>userProfile</STRONG><BR>
+An url to the profile of the creator of this search result.</P>
+<P><STRONG>header</STRONG><BR>The title of the search result. (This can be the 
 subject of a message, the question of a FAQ, the title of an Article, etc)</P>
-<P><STRONG>body<BR></STRONG>A preview of the content of the search result.</P>
-<P><STRONG>namespace<BR></STRONG>The namespace in which this search result 
+<P><STRONG>body</STRONG><BR>A preview of the content of the search result.</P>
+<P><STRONG>namespace</STRONG><BR>The namespace in which this search result 
 resides.</P>
-<P><STRONG>location<BR></STRONG>The URL of this search result.</P>
-<P><STRONG>crumbtrail<BR></STRONG>A crumbtrail to this search result.</P>
-<P><STRONG>contentType<BR></STRONG>The type of this search 
+<P><STRONG>location</STRONG><BR>The URL of this search result.</P>
+<P><STRONG>crumbtrail</STRONG><BR>A crumbtrail to this search result.</P>
+<P><STRONG>contentType</STRONG><BR>The type of this search 
 result.</P></BLOCKQUOTE>
 <P dir=ltr>The loops <STRONG>contentTypes</STRONG>, 
 <STRONG>contentTypesSimple</STRONG>, <STRONG>languages</STRONG>, 
-<STRONG>namespaces</STRONG> and&nbsp;<STRONG>users </STRONG>all look the same. 
+<STRONG>namespaces</STRONG> and <STRONG>users</STRONG> all look the same. 
 They can be used to create a select list, radio list or check list so users can 
 refine their search.</P>
-<P dir=ltr>This tempate variables are available inside the loops:</P>
+<P dir=ltr>This template variables are available inside the loops:</P>
 <BLOCKQUOTE dir=ltr style="MARGIN-RIGHT: 0px">
-<P dir=ltr><STRONG>name<BR></STRONG>The (possibly internationalized) name of the 
+<P dir=ltr><STRONG>name</STRONG><BR>The (possibly internationalized) name of the 
 option.<BR><BR><STRONG>value<BR></STRONG>The value of the 
 option.<BR><BR><STRONG>selected<BR></STRONG>A conditional indicating whether 
 this option is selected or not.</P></BLOCKQUOTE>
@@ -180,8 +189,10 @@ this option is selected or not.</P></BLOCKQUOTE>
 <b>value</b><br>The value you should pass as a form param.<br><br>
 <b>checked</b><br>True if this pageroot is selected.<br><br>
 </p></blockquote>
-<p dir=ltr><b>rootPage.<i>urlizedTitle</i>.id</b><br>This is a direct link to the value property of the rootpage identified with <i>urlizedTitle</i> that is also given by the value property of the <B>searchRoots</B> loop.</p>
-<p dir=ltr><b>rootPage.<i>urlizedTitle</i>.checked</b><br>This is a direct link to the checked property of the rootpage identified with <i>urlizedTitle</i> that is also given by the checked property of the <B>searchRoots</B> loop.</p>
+<p dir=ltr><b>rootPage.<i>urlizedTitle</i>.id</b><br>
+This is a direct link to the value property of the rootpage identified with <i>urlizedTitle</i> that is also given by the value property of the <B>searchRoots</B> loop.</p>
+<p dir=ltr><b>rootPage.<i>urlizedTitle</i>.checked</b><br>
+This is a direct link to the checked property of the rootpage identified with <i>urlizedTitle</i> that is also given by the checked property of the <B>searchRoots</B> loop.</p>
 <P><B>firstPage</B><BR>A link to the first page in the paginator. 
 <P><B>lastPage</B><BR>A link to the last page in the paginator. 
 <P><B>nextPage</B><BR>A link to the next page forward in the paginator. 
@@ -193,7 +204,7 @@ one page in the paginator.
 the first page. 
 <P><B>isLastPage</B><BR>A conditional indicating whether the visitor is viewing 
 the last page.</P>|,
-		lastUpdated => 1090580644
+		lastUpdated => 1101773812
 	},
 
 	'3' => {
