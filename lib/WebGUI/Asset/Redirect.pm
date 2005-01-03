15c48e14d05449b3fe82643bf0eaf1f17b95732e
Colin Kuskie <colink@perldreamer.com>
2005-01-03 02:54:02 +0000

    internationalize Redirect URL property

--- lib/WebGUI/Asset/Redirect.pm
+++ lib/WebGUI/Asset/Redirect.pm
@@ -86,7 +86,7 @@ sub getEditForm {
 	my $tabform = $self->SUPER::getEditForm();
         $tabform->getTab("properties")->url(
                 -name=>"redirectUrl",
-                -label=>"Redirect URL",
+                -label=>WebGUI::International::get('redirect url', 'Asset'),
                 -value=>$self->getValue("redirectUrl")
                 );
 	return $tabform;
