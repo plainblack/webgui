302ab8ad17008dd41ec07a4b68d3cd655292f2a9
Colin Kuskie <colink@perldreamer.com>
2005-02-28 19:30:56 +0000

    internationalize template labels a-e

--- lib/WebGUI/i18n/English/Collaboration.pm
+++ lib/WebGUI/i18n/English/Collaboration.pm
@@ -1,6 +1,86 @@
 package WebGUI::i18n::English::Collaboration;
 
 our $I18N = {
+	'add' => {
+		message => q|Add|,
+		lastUpdated => 1109618544,
+	},
+
+	'addlink' => {
+		message => q|Add a link|,
+		lastUpdated => 1109618544,
+	},
+
+	'addquestion' => {
+		message => q|Add a question|,
+		lastUpdated => 1109618544,
+	},
+
+	'all' => {
+		message => q|All Words|,
+		lastUpdated => 1109618544,
+	},
+
+	'atleastone' => {
+		message => q|At Least One|,
+		lastUpdated => 1109618544,
+	},
+
+	'approve' => {
+		message => q|Approve|,
+		lastUpdated => 1109618544,
+	},
+
+	'answer' => {
+		message => q|Answer|,
+		lastUpdated => 1109618544,
+	},
+
+	'attachment' => {
+		message => q|Attachment|,
+		lastUpdated => 1109618544,
+	},
+
+	'by' => {
+		message => q|By|,
+		lastUpdated => 1109618544,
+	},
+
+	'body' => {
+		message => q|Body|,
+		lastUpdated => 1109618544,
+	},
+
+	'back' => {
+		message => q|Back|,
+		lastUpdated => 1109618544,
+	},
+
+	'contentType' => {
+		message => q|Content Type|,
+		lastUpdated => 1109618544,
+	},
+
+	'date' => {
+		message => q|Date|,
+		lastUpdated => 1109618544,
+	},
+
+	'delete' => {
+		message => q|Delete|,
+		lastUpdated => 1109618544,
+	},
+
+	'description' => {
+		message => q|Description|,
+		lastUpdated => 1109618544,
+	},
+
+	'deny' => {
+		message => q|Deny|,
+		lastUpdated => 1109618544,
+	},
+
 
 };
 
