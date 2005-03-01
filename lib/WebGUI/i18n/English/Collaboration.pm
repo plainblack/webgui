5ef3b565f734a0372a8d7e9deb80763e5f7971eb
Colin Kuskie <colink@perldreamer.com>
2005-03-01 17:17:06 +0000

    internationalize template labels m-z

--- lib/WebGUI/i18n/English/Collaboration.pm
+++ lib/WebGUI/i18n/English/Collaboration.pm
@@ -106,6 +106,11 @@ our $I18N = {
 		lastUpdated => 1109618544,
 	},
 
+	'edit link' => {
+		message => q|Edit Link|,
+		lastUpdated => 1109697313,
+	},
+
 	'lastReply' => {
 		message => q|Last Reply|,
 		lastUpdated => 1109618544,
@@ -119,6 +124,176 @@ our $I18N = {
 	'layout' => {
 		message => q|Layout|,
 		lastUpdated => 1109618544,
+	},
+
+	'edit message' => {
+		message => q|Edit Message|,
+		lastUpdated => 1109696027,
+	},
+
+	'message' => {
+		message => q|Message|,
+		lastUpdated => 1109696029,
+	},
+
+	'next' => {
+		message => q|Next|,
+		lastUpdated => 1109696029,
+	},
+
+	'new window' => {
+		message => q|Open in new window?|,
+		lastUpdated => 1109696029,
+	},
+
+	'nested' => {
+		message => q|Nested|,
+		lastUpdated => 1109696029,
+	},
+
+	'previous' => {
+		message => q|Previous|,
+		lastUpdated => 1109696029,
+	},
+
+	'post' => {
+		message => q|Post|,
+		lastUpdated => 1109697351,
+	},
+
+	'question' => {
+		message => q|Question|,
+		lastUpdated => 1109696720,
+	},
+
+	'edit question' => {
+		message => q|Edit Question|,
+		lastUpdated => 1109696722,
+	},
+
+	'rating' => {
+		message => q|Rating|,
+		lastUpdated => 1109696029,
+	},
+
+	'rate' => {
+		message => q|Rate|,
+		lastUpdated => 1109696029,
+	},
+
+	'reply' => {
+		message => q|Reply|,
+		lastUpdated => 1109696029,
+	},
+
+	'replies' => {
+		message => q|Replies|,
+		lastUpdated => 1109696029,
+	},
+
+	'read more' => {
+		message => q|Read More|,
+		lastUpdated => 1109696029,
+	},
+
+	'responses' => {
+		message => q|Responses|,
+		lastUpdated => 1109696029,
+	},
+
+	'results' => {
+		message => q|Results|,
+		lastUpdated => 1109696029,
+	},
+
+	'search' => {
+		message => q|Search|,
+		lastUpdated => 1109696029,
+	},
+
+	'subject' => {
+		message => q|Subject|,
+		lastUpdated => 1109696029,
+	},
+
+	'subscribe' => {
+		message => q|Subscribe|,
+		lastUpdated => 1109696029,
+	},
+
+	'edit submission' => {
+		message => q|Edit Submission|,
+		lastUpdated => 1109696029,
+	},
+
+	'start date' => {
+		message => q|Start Date|,
+		lastUpdated => 1109696029,
+	},
+
+	'sticky' => {
+		message => q|Make Sticky|,
+		lastUpdated => 1109697033,
+	},
+
+	'status' => {
+		message => q|Status|,
+		lastUpdated => 1109697030,
+	},
+
+	'thumbnail' => {
+		message => q|Thumbnail|,
+		lastUpdated => 1109696029,
+	},
+
+	'title' => {
+		message => q|Title|,
+		lastUpdated => 1109696029,
+	},
+
+	'threaded' => {
+		message => q|Threaded|,
+		lastUpdated => 1109696029,
+	},
+
+	'unlock' => {
+		message => q|Unlock|,
+		lastUpdated => 1109696029,
+	},
+
+	'unstick' => {
+		message => q|Unstick|,
+		lastUpdated => 1109696029,
+	},
+
+	'unsubscribe' => {
+		message => q|Unsubscribe|,
+		lastUpdated => 1109696029,
+	},
+
+	'url' => {
+		message => q|URL|,
+		lastUpdated => 1109696029,
+	},
+
+	'user' => {
+		message => q|User|,
+		lastUpdated => 1109696029,
+	},
+
+	'views' => {
+		message => q|Views|,
+		lastUpdated => 1109696029,
+	},
+
+	'visitor' => {
+		message => q|Visitor Name|,
+		lastUpdated => 1109696029,
+	},
+
+	'without' => {
+		message => q|Without|,
+		lastUpdated => 1109696029,
 	},
 
 };
