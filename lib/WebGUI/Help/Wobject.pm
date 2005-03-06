bd6d045000199d27a225b85551164818301823f1
Colin Kuskie <colink@perldreamer.com>
2005-03-06 17:49:33 +0000

    Help file for Wobject

---
+++ lib/WebGUI/Help/Wobject.pm
@@ -0,0 +1,150 @@
+package WebGUI::Help::Wobject;
+
+our $HELP = {
+
+	'wobjects using' => {
+		title => '671',
+		body => '626',
+		related => [
+			{
+				tag => 'macros using',
+				namespace => 'WebGUI'
+			},
+			{
+				tag => 'style sheets using',
+				namespace => 'WebGUI'
+			},
+			{
+				tag => 'wobject add/edit',
+				namespace => 'Wobject'
+			},
+			{
+				tag => 'wobject delete',
+				namespace => 'Wobject'
+			}
+		]
+	},
+
+	'wobject add/edit' => {
+		title => '677',
+		body => '632',
+		related => [
+			{
+				tag => 'asset fields',
+				namespace => 'Asset'
+			},
+			{
+				tag => 'article add/edit',
+				namespace => 'Article'
+			},
+			{
+				tag => 'events calendar add/edit',
+				namespace => 'EventsCalendar'
+			},
+			{
+				tag => 'http proxy add/edit',
+				namespace => 'HttpProxy'
+			},
+			{
+				tag => 'data form add/edit',
+				namespace => 'DataForm'
+			},
+			{
+				tag => 'message board add/edit',
+				namespace => 'MessageBoard'
+			},
+			{
+				tag => 'metadata manage',
+				namespace => 'Asset'
+			},
+			{
+				tag => 'poll add/edit',
+				namespace => 'Poll'
+			},
+			{
+				tag => 'product add/edit',
+				namespace => 'Product'
+			},
+			{
+				tag => 'sql report add/edit',
+				namespace => 'SQLReport'
+			},
+			{
+				tag => 'survey add/edit',
+				namespace => 'Survey'
+			},
+			{
+				tag => 'syndicated content add/edit',
+				namespace => 'SyndicatedContent'
+			},
+			{
+				tag => 'wobjects using',
+				namespace => 'Wobject'
+			}
+		]
+	},
+
+	'wobject delete' => {
+		title => '664',
+		body => '619',
+		related => [
+			{
+				tag => 'wobjects using',
+				namespace => 'Wobject'
+			}
+		]
+	},
+
+	'wobject template' => {
+		title => '827',
+		body => '828',
+		related => [
+			{
+				tag => 'article template',
+				namespace => 'Article'
+			},
+			{
+				tag => 'data form template',
+				namespace => 'DataForm'
+			},
+			{
+				tag => 'events calendar template',
+				namespace => 'EventsCalendar'
+			},
+			{
+				tag => 'message board template',
+				namespace => 'MessageBoard'
+			},
+			{
+				tag => 'pagination template variables',
+				namespace => 'WebGUI'
+			},
+			{
+				tag => 'poll template',
+				namespace => 'Poll'
+			},
+			{
+				tag => 'product template',
+				namespace => 'Product'
+			},
+			{
+				tag => 'survey template',
+				namespace => 'Survey'
+			},
+			{
+				tag => 'syndicated content template',
+				namespace => 'SyndicatedContent'
+			},
+			{
+				tag => 'template language',
+				namespace => 'Template'
+			},
+			{
+				tag => 'templates manage',
+				namespace => 'Template'
+			},
+		]
+	},
+};
+
+1;
