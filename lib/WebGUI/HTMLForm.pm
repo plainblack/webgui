09b888712f6df3c3dcbeffbe2a812227dddc03df
Colin Kuskie <colink@perldreamer.com>
2005-03-16 04:41:51 +0000

    typo in fieldType rearrange

--- lib/WebGUI/HTMLForm.pm
+++ lib/WebGUI/HTMLForm.pm
@@ -1022,7 +1022,7 @@ sub fieldType {
         my ($output);
         my ($self, @p) = @_;
         my ($name, $types, $label, $value, $size, $multiple, $extras, $subtext, $uiLevel, $defaultValue) =
-                rearrange([qw(name types label value size multiple extras subtext uiLevel $defaultValue)], @p);
+                rearrange([qw(name types label value size multiple extras subtext uiLevel defaultValue)], @p);
         if (_uiLevelChecksOut($uiLevel)) {
                 $output = WebGUI::Form::fieldType({
                         "name"=>$name,
