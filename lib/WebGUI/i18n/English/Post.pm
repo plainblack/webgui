43602a7ef1110e028d5cfdb2a45d7f23c3c476f3
Colin Kuskie <colink@perldreamer.com>
2005-03-21 06:53:56 +0000

    document template for add/edit a post

--- lib/WebGUI/i18n/English/Post.pm
+++ lib/WebGUI/i18n/English/Post.pm
@@ -41,8 +41,108 @@ The content of the Post that is being replied to.
 A yes/no button to allow the user to subscribe to the thread this post belongs to.
 <p>
 
+<b>isNewThread</b><br>
+A conditional that is true if the user is adding a new thread.
+<p>
+
+<b>sticky.form</b><br>
+A yes/no button to set the thread to be sticky.
+<p>
+
+<b>lock.form</b><br>
+A yes/no button to lock the thread.
+<p>
+
+<b>isEdit</b><br>
+A conditional that is true if the user is editing an existing post.
+<p>
+
+<b>preview.title</b><br>
+The web safe title for previewing a post.
+<p>
+
+<b>preview.synopsis</b><br>
+The synopsis when previewing a post.
+<p>
+
+<b>preview.content</b><br>
+The content when previewing a post.
+<p>
+
+<b>preview.userDefined.<i>N</i></b><br>
+The contents of user defined fields for the Post without WebGUI Macros being processed, where N is from 1 to 5.
+<p>
+
+<b>form.footer</b> !<br>
+Code for the end of the form.
+<p>
+
+<b>usePreview</b> !<br>
+A conditional indicating that posts to the thread will be previewed before being submitted.
+<p>
+
+<b>user.isModerator</b><br>
+A conditional indicating if the current user is a moderator.
+<p>
+
+<b>user.isVisitor</b><br>
+A conditional indicating if the current user is a visitor.
+<p>
+
+<b>visitorName.form</b><br>
+A form where the user can enter their name.
+<p>
+
+<b>userDefined.<i>N</i>.{form,yesNo,textarea,htmlarea}</b><br>
+For each of the 5 User Defined fields, form widgets for a single line of text, a yes/no
+field, a text area, or a WYSIWIG HTML area.
+<p>
+
+<b>title.form</b><br>
+A form field to enter or edit the title, stripped of all HTML and macros disabled.
+<p>
+
+<b>title.form.textarea</b><br>
+A text field to enter or edit the title, stripped of all HTML and macros disabled.
+<p>
+
+<b>synopsis.form</b><br>
+A form field to enter or edit the synopsis.
+<p>
+
+<b>content.form</b><br>
+A field to enter or edit the content, with all macros disabled.  If the discussion
+board allows rich content, then this will be a WYSIWIG HTML area.  Otherwise it
+will be a plain text area.
+<p>
+
+<b>form.submit</b><br>
+A button to submit the post.
+<p>
+
+<b>form.preview</b><br>
+A button to preview the post.
+<p>
+
+<b>attachment.form</b><br>
+Code to allow an attachment to be added to the post.
+<p>
+
+<b>contentType.form</b><br>
+A form field that will describe how the content of the post is formatted, HTML, text, code or mixed.
+Defaults to mixed.
+<p>
+
+<b>startDate.form</b><br>
+A form that will set when the post starts to be available.
+<p>
+
+<b>endDate.form</b><br>
+A form that will set when the post stops being available.
+<p>
+
 |,
-		lastUpdated => 1111252633,
+		lastUpdated => 1111388442,
 	},
 
 };
