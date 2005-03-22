4a65af3b9fbf199666b503188e73690abca6156d
Colin Kuskie <colink@perldreamer.com>
2005-03-22 23:35:18 +0000

    document Collaboration template labels

--- lib/WebGUI/Help/Collaboration.pm
+++ lib/WebGUI/Help/Collaboration.pm
@@ -1,6 +1,12 @@
 package WebGUI::Help::Collaboration;
 
 our $HELP = {
+	'collaboration template labels' => {
+		title => 'collaboration template labels title',
+		body => 'collaboration template labels body',
+		related => [
+		]
+	},
 
 };
 
