30b05c41c488c869c6351663ce2d36b97cba8bc9
Colin Kuskie <colink@perldreamer.com>
2005-04-03 18:03:20 +0000

    document macros for subscription items

---
+++ lib/WebGUI/i18n/English/SubscriptionItemPurchaseUrl.pm
@@ -0,0 +1,21 @@
+package WebGUI::i18n::English::SubscriptionItemPurchaseUrl;
+
+our $I18N = {
+
+    'subscription item purchase url title' => {
+        message => q|Subscription Item Purchase URL Macro|,
+        lastUpdated => 1112548245,
+    },
+
+	'subscription item purchase url body' => {
+		message => q|
+
+<b>&#94;SubscriptionItemPurchaseUrl(<i>subscriptionId</i>);</b><br>
+This macro fetches the URL to purchase a subscription item identified
+by its subscriptionId.
+|,
+		lastUpdated => 1112548242,
+	},
+};
+
+1;
