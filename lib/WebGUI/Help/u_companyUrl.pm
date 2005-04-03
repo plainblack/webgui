359dc92e0ae9d13bf256dac385cc18f1d4502ba5
Colin Kuskie <colink@perldreamer.com>
2005-04-03 07:40:51 +0000

    individual help for macros

---
+++ lib/WebGUI/Help/u_companyUrl.pm
@@ -0,0 +1,18 @@
+package WebGUI::Help::u_companyUrl;
+
+our $HELP = {
+
+        'company url' => {
+		title => 'company url title',
+		body => 'company url body',
+		related => [
+			{
+				tag => 'macros using',
+				namespace => 'WebGUI'
+			},
+		]
+	},
+
+};
+
+1;
---
+++ lib/WebGUI/i18n/English/u_companyUrl.pm
@@ -0,0 +1,22 @@
+package WebGUI::i18n::English::u_companyUrl;
+
+our $I18N = {
+
+    'company url title' => {
+        message => q|Company Url Macro|,
+        lastUpdated => 1112466408,
+    },
+
+	'company url body' => {
+		message => q|
+
+<b>&#94;u; - Company URL</b><br>
+The URL for your company specified in the settings by your Administrator.
+<p>
+
+|,
+		lastUpdated => 1112466919,
+	},
+};
+
+1;
