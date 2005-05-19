9db31c95d686934b3556d1a68abdf24b20874b82
Frank Dillon <frank@plainblack.com>
2005-05-19 00:26:11 +0000

    Added columns to group to store ldap group settings.

--- docs/upgrades/upgrade_6.6.0-6.6.1.sql
+++ docs/upgrades/upgrade_6.6.0-6.6.1.sql
@@ -1,3 +1,6 @@
 insert into webguiVersion values ('6.6.1','upgrade',unix_timestamp());
 insert into settings values ('commerceSendDailyReportTo', '');
 ALTER TABLE navigation CHANGE COLUMN anscestorEndPoint ancestorEndPoint INTEGER NOT NULL DEFAULT 55;
+alter table groups add ldapGroup varchar(255) default NULL;
+alter table groups add ldapGroupProperty varchar(255) default NULL;
+alter table groups add ldapRecursiveProperty varchar(255) default NULL; 
