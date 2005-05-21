a3674ff8190f92e7d5974937da386a4a4a316098
Matthew Wilson <matt@plainblack.com>
2005-05-21 13:07:00 +0000

    *** empty log message ***

--- docs/upgrades/upgrade_6.6.0-6.6.1.sql
+++ docs/upgrades/upgrade_6.6.0-6.6.1.sql
@@ -3,4 +3,4 @@ insert into settings values ('commerceSendDailyReportTo', '');
 alter table Navigation change anscestorEndPoint ancestorEndPoint int not null default 55;
 alter table groups add ldapGroup varchar(255) default NULL;
 alter table groups add ldapGroupProperty varchar(255) default NULL;
-alter table groups add ldapRecursiveProperty varchar(255) default NULL; >>>>>>> 1.4
+alter table groups add ldapRecursiveProperty varchar(255) default NULL;
