1c081b67df6a36e2dff51c83098f154ab69ebbbb
Matthew Wilson <matt@plainblack.com>
2005-05-25 02:32:31 +0000

    slight typo.

--- lib/WebGUI/Macro/H_homeLink.pm
+++ lib/WebGUI/Macro/H_homeLink.pm
@@ -33,7 +33,7 @@ sub process {
        		if ($param[0] ne "") {
                		$var{'homeLink.text'} = $param[0];
        		} else {
-               		$var{'homeLink.text'} = WebGUI::International::get(47,'Macro_H_homelink');
+               		$var{'homeLink.text'} = WebGUI::International::get(47,'Macro_H_homeLink');
        		}
 		if ($param[1]) {
          		$temp =  WebGUI::Asset::Template->newByUrl($param[1])->process(\%var);
