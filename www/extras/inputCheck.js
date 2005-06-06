fdeb18009654aa170a5f4208913665bce7e25823
Roy Johnson <roy.l.johnson@gmail.com>
2005-06-06 20:18:15 +0000

    fix - [ 1195523 ] Bug in Data Type Telephone Number

--- www/extras/inputCheck.js
+++ www/extras/inputCheck.js
@@ -4,9 +4,9 @@ function doInputCheck(field,valid) {
         for (var i=0; i<field.value.length; i++) {
         	temp = "" + field.value.substring(i, i+1);
                 if (valid.indexOf(temp) == "-1") ok = "no";
-        }
-        if (ok == "no") {
-                field.value = field.value.substring(0, (field.value.length) - 1);
+        
+        	if (ok == "no") {
+        	 field.value = field.value.substring(0, i);
+		}
         }
 }
-
