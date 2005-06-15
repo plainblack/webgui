ccb0ea63e547da74897b2988a0ac63ae19dc5715
Daniel Collis-Puro <geekuprising@webgui-cvs>
2005-06-15 15:34:07 +0000

    Fixing my mistakes in "upgrade_6.6.1-6.6.2.sql" and removing my updateTranslations.pl utility.

--- docs/upgrades/upgrade_6.6.3-6.7.0.sql
+++ docs/upgrades/upgrade_6.6.3-6.7.0.sql
@@ -1,2 +1,4 @@
 insert into webguiVersion values ('6.7.0','upgrade',unix_timestamp());
+alter table SyndicatedContent add column displayMode varchar(20) not null default 'interleaved';
+alter table SyndicatedContent add column hasTerms varchar(255) not null;
 
