7aec8869b1a7f089679ca310338d69528cd0b162
Wouter van Oijen <wouter@webgui-svn>
2005-09-01 18:56:41 +0000

    Oops, forget the small assets.gif. Fixed that now too.

--- lib/WebGUI/Asset.pm
+++ lib/WebGUI/Asset.pm
@@ -693,6 +693,7 @@ sub getIcon {
 	my $small = shift;
 	my $definition = $self->definition;
 	my $icon = $definition->[0]{icon} || "assets.gif";
+	return $session{config}{extrasURL}."/adminConsole/small/assets.gif" if ($small and $icon eq "assets.gif");
 	return $session{config}{extrasURL}."/adminConsole/assets.gif" if ($icon eq "assets.gif");
 	return $session{config}{extrasURL}.'/assets/small/'.$icon if ($small);
 	return $session{config}{extrasURL}.'/assets/'.$icon;
