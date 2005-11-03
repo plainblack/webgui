774641ebe6786734dd7f0aa8d01662dd9b5076f3
JT Smith <jt@plainblack.com>
2005-11-03 09:21:20 +0000

    moving date time stuff into it's own i18n file

--- lib/WebGUI/i18n/English/DateTime.pm
+++ lib/WebGUI/i18n/English/DateTime.pm
@@ -2,100 +2,106 @@ package WebGUI::i18n::English::DateTime;
 
 our $I18N = {
 
-	'sunday' => {
-		message => q|Sunday|,
-		lastUpdated => 1130998595,
+	'timezone' => {
+		message => q|Time Zone|,
+		lastUpdated => 0
 	},
 
 	'monday' => {
 		message => q|Monday|,
-		lastUpdated => 1130998613,
+		lastUpdated => 1031514049
 	},
 
 	'tuesday' => {
 		message => q|Tuesday|,
-		lastUpdated => 1130998614,
+		lastUpdated => 1031514049
 	},
 
 	'wednesday' => {
 		message => q|Wednesday|,
-		lastUpdated => 1130998632,
+		lastUpdated => 1031514049
 	},
 
 	'thursday' => {
 		message => q|Thursday|,
-		lastUpdated => 1130998634,
+		lastUpdated => 1031514049
 	},
 
 	'friday' => {
 		message => q|Friday|,
-		lastUpdated => 1130998648,
+		lastUpdated => 1031514049
 	},
 
 	'saturday' => {
 		message => q|Saturday|,
-		lastUpdated => 1130998680,
+		lastUpdated => 1031514049
+	},
+
+	'sunday' => {
+		message => q|Sunday|,
+		lastUpdated => 1031514049
 	},
 
 	'january' => {
 		message => q|January|,
-		lastUpdated => 1130998687,
+		lastUpdated => 1031514049
 	},
 
 	'february' => {
 		message => q|February|,
-		lastUpdated => 1130998695,
+		lastUpdated => 1031514049
 	},
 
 	'march' => {
 		message => q|March|,
-		lastUpdated => 1130998702,
+		lastUpdated => 1031514049
 	},
 
 	'april' => {
 		message => q|April|,
-		lastUpdated => 1130998708,
+		lastUpdated => 1031514049
 	},
 
 	'may' => {
 		message => q|May|,
-		lastUpdated => 1130998712,
+		lastUpdated => 1031514049
 	},
 
 	'june' => {
 		message => q|June|,
-		lastUpdated => 1130998723,
+		lastUpdated => 1031514049
 	},
 
 	'july' => {
 		message => q|July|,
-		lastUpdated => 1130998729,
+		lastUpdated => 1031514049
 	},
 
 	'august' => {
 		message => q|August|,
-		lastUpdated => 1130998736,
+		lastUpdated => 1031514049
 	},
 
 	'september' => {
 		message => q|September|,
-		lastUpdated => 1130998744,
+		lastUpdated => 1031514049
 	},
 
 	'october' => {
 		message => q|October|,
-		lastUpdated => 1130998754,
+		lastUpdated => 1031514049
 	},
 
 	'november' => {
 		message => q|November|,
-		lastUpdated => 1130998763,
+		lastUpdated => 1031514049
 	},
-
+        
 	'december' => {
 		message => q|December|,
-		lastUpdated => 1130998769,
+		lastUpdated => 1031514049
 	},
+
 };
 
 1;
