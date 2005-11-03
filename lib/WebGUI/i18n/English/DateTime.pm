ec9d561c5aef104f7bb7c751e4012a9e0ac166ba
Colin Kuskie <colink@perldreamer.com>
2005-11-03 06:15:07 +0000

    day and month labels for DateTime

---
+++ lib/WebGUI/i18n/English/DateTime.pm
@@ -0,0 +1,101 @@
+package WebGUI::i18n::English::DateTime;
+
+our $I18N = {
+
+	'sunday' => {
+		message => q|Sunday|,
+		lastUpdated => 1130998595,
+	},
+
+	'monday' => {
+		message => q|Monday|,
+		lastUpdated => 1130998613,
+	},
+
+	'tuesday' => {
+		message => q|Tuesday|,
+		lastUpdated => 1130998614,
+	},
+
+	'wednesday' => {
+		message => q|Wednesday|,
+		lastUpdated => 1130998632,
+	},
+
+	'thursday' => {
+		message => q|Thursday|,
+		lastUpdated => 1130998634,
+	},
+
+	'friday' => {
+		message => q|Friday|,
+		lastUpdated => 1130998648,
+	},
+
+	'saturday' => {
+		message => q|Saturday|,
+		lastUpdated => 1130998680,
+	},
+
+	'january' => {
+		message => q|January|,
+		lastUpdated => 1130998687,
+	},
+
+	'february' => {
+		message => q|February|,
+		lastUpdated => 1130998695,
+	},
+
+	'march' => {
+		message => q|March|,
+		lastUpdated => 1130998702,
+	},
+
+	'april' => {
+		message => q|April|,
+		lastUpdated => 1130998708,
+	},
+
+	'may' => {
+		message => q|May|,
+		lastUpdated => 1130998712,
+	},
+
+	'june' => {
+		message => q|June|,
+		lastUpdated => 1130998723,
+	},
+
+	'july' => {
+		message => q|July|,
+		lastUpdated => 1130998729,
+	},
+
+	'august' => {
+		message => q|August|,
+		lastUpdated => 1130998736,
+	},
+
+	'september' => {
+		message => q|September|,
+		lastUpdated => 1130998744,
+	},
+
+	'october' => {
+		message => q|October|,
+		lastUpdated => 1130998754,
+	},
+
+	'november' => {
+		message => q|November|,
+		lastUpdated => 1130998763,
+	},
+
+	'december' => {
+		message => q|December|,
+		lastUpdated => 1130998769,
+	},
+};
+
+1;
