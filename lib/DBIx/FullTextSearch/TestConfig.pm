a5a99374dea58ba0f40e0effe0b0d3e083132c3a
JT Smith <jt@plainblack.com>
2005-11-12 03:01:55 +0000

    don't need this anymore

--- lib/DBIx/FullTextSearch/TestConfig.pm
+++
@@ -1,6 +0,0 @@
-%DBIx::FullTextSearch::TestConfig::Config = (
-                                              'password' => undef,
-                                              'dsn' => 'dbi:mysql:test',
-                                              'user' => 'test'
-                                            );
-1;
