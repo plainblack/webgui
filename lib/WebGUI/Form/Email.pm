6a89651b12b409531d10e2b3336c4d6292eac6a5
Matthew Wilson <matt@plainblack.com>
2005-11-19 22:38:13 +0000

    spacing.

--- lib/WebGUI/Form/Email.pm
+++ lib/WebGUI/Form/Email.pm
@@ -89,10 +89,11 @@ Returns a validated email address. If the result does not pass validation, it re
 sub getValueFromPost {
 	my $self = shift;
 	my $value = $session{req}->param($self->{name});
-   	if ($value =~ /^([A-Z0-9]+[._+-]?){1,}([A-Z0-9]+[_+-]?)+\@(([A-Z0-9]+[._-]?){1,}[A-Z0-9]+\.){1,}[A-Z]{2,4}$/i) {
-                return $value;
-        }
-        return undef;
+
+	if ($value =~ /^([A-Z0-9]+[._+-]?){1,}([A-Z0-9]+[_+-]?)+\@(([A-Z0-9]+[._-]?){1,}[A-Z0-9]+\.){1,}[A-Z]{2,4}$/i) {
+		return $value;
+	}
+	return undef;
 }
 
 #-------------------------------------------------------------------
