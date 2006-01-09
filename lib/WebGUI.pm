748aaf22e7edae3f8c9a03257d32a26b6d48345d
JT Smith <jt@plainblack.com>
2006-01-09 23:43:06 +0000

    changing copyright year

--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -3,7 +3,7 @@ our $VERSION = "6.8.4";
 our $STATUS = "gamma";
 
 #-------------------------------------------------------------------
-# WebGUI is Copyright 2001-2005 Plain Black Corporation.
+# WebGUI is Copyright 2001-2006 Plain Black Corporation.
 #-------------------------------------------------------------------
 # Please read the legal notices (docs/legal.txt) and the license
 # (docs/license.txt) that came with this distribution before using
