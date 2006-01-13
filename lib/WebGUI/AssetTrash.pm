62f5ad3ed9a7d95628ab87a1d9e2b0e2a4d117fa
Colin Kuskie <colink@perldreamer.com>
2006-01-13 00:39:33 +0000

    fix typo

--- lib/WebGUI/AssetTrash.pm
+++ lib/WebGUI/AssetTrash.pm
@@ -124,7 +124,7 @@ sub trash {
 	return undef if ($self->getId eq $self->session->setting->get("defaultPage") || $self->getId eq $self->session->setting->get("notFoundPage"));
 	$self->session->db->beginTransaction;
 	$self->session->db->write("update asset set state='trash-limbo' where lineage like ".$self->session->db->quote($self->get("lineage").'%'));
-	$self->session->db->write("update asset set state='trash', stateChangedBy=".$self->session->db->quote($self->session->user->profileField("userId")).", stateChanged="$self->session->datetime->time()." where assetId=".$self->session->db->quote($self->getId));
+	$self->session->db->write("update asset set state='trash', stateChangedBy=".$self->session->db->quote($self->session->user->profileField("userId")).", stateChanged=".$self->session->datetime->time()." where assetId=".$self->session->db->quote($self->getId));
 	$self->session->db->commit;
 	$self->{_properties}{state} = "trash";
 	$self->updateHistory("trashed");
