f55e487d3ec9e4a4d6fa743439f9104db4b9dbb9
Colin Kuskie <colink@perldreamer.com>
2006-01-14 00:16:26 +0000

    restoring lost/emptied file

---
+++ lib/WebGUI/Help/Macro_FetchMimeType.pm
@@ -0,0 +1,20 @@
+package WebGUI::Help::Macro_FetchMimeType;
+
+our $HELP = {
+
+        'fetch mime type' => {
+		title => 'fetch mime type title',
+		body => 'fetch mime type body',
+		fields => [
+		],
+		related => [
+			{
+				tag => 'macros using',
+				namespace => 'Macros'
+			},
+		]
+	},
+
+};
+
+1;
