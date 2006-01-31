80c859cf52089e9f44a915bfc2153faafbb37000
JT Smith <jt@plainblack.com>
2006-01-31 03:59:32 +0000

    forgot to add this before

---
+++ lib/WebGUI/i18n/English/Asset_Search.pm
@@ -0,0 +1,18 @@
+package WebGUI::i18n::English::Asset_Search;  ##Be sure to change the package name to match the filename
+
+our $I18N = { ##hashref of hashes
+
+	'search' => {
+		message => q|search|,
+		lastUpdated => 0,
+		context => q|A label to place on the search button.|
+	},
+
+	'assetName' => {
+		message => q|Search|,
+		lastUpdated => 0,
+	},
+
+};
+
+1;
