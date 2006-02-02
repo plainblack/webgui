21c4d5a93cf24f807bb3f9ad3bd30a3e57692673
Colin Kuskie <colink@perldreamer.com>
2006-02-02 17:30:45 +0000

    l10n for Search

--- lib/WebGUI/i18n/English/Asset_Search.pm
+++ lib/WebGUI/i18n/English/Asset_Search.pm
@@ -13,6 +13,42 @@ our $I18N = { ##hashref of hashes
 		lastUpdated => 0,
 	},
 
+	'search template' => {
+		message => q|Search Template|,
+		lastUpdated => 1138900894,
+		context => q|form field in add/edit search form|
+	},
+
+	'search template description' => {
+		message => q|A template to display the search form and results.|,
+		lastUpdated => 1138900894,
+		context => q|hover help for search template|
+	},
+
+	'search root' => {
+		message => q|Search Root|,
+		lastUpdated => 1138900894,
+		context => q|form field in add/edit search form|
+	},
+
+	'search root description' => {
+		message => q|The Asset you select, and all Assets below it, will be searched.|,
+		lastUpdated => 1138900894,
+		context => q|hover help for search root|
+	},
+
+	'class limiter' => {
+		message => q|Limit Asset classes to:|,
+		lastUpdated => 1138900894,
+		context => q|form field in add/edit search form|
+	},
+
+	'class limiter description' => {
+		message => q|This will limit the types of Assets that are searched to only those that you select.|,
+		lastUpdated => 1138900894,
+		context => q|hover help for search root|
+	},
+
 };
 
 1;
