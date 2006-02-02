929df9cbf9d9e304ba22d30b1e0b383e4e8b286e
Colin Kuskie <colink@perldreamer.com>
2006-02-02 05:16:54 +0000

    new l10n entries

--- lib/WebGUI/i18n/English/Asset_EventManagementSystem.pm
+++ lib/WebGUI/i18n/English/Asset_EventManagementSystem.pm
@@ -13,6 +13,18 @@ our $I18N = { ##hashref of hashes
 		context => q|Describes this template field selector|
 	},
 
+	'event template' => { 
+		message => q|Event Template|,
+		lastUpdated => 1131394070, #seconds from the epoch
+		context => q|Field label for event template selector|
+	},
+
+	'display template description' => {
+		message => q|Controls the layout, look, and appearence of an individual Event in the Event Management System.|,
+		lastUpdated => 1131394072,
+		context => q|Describes the event template field selector|
+	},
+
 	'paginate after' => {
 		message => q|Paginate After|,
 		lastUpdated => 1131394072,
@@ -36,6 +48,30 @@ our $I18N = { ##hashref of hashes
 		Events added will not be available for purchase until the event is approved by a member of the Group to Approve Events.|,
 		lastUpdated => 1131394072,
 		context => q|Describes the Group To Add Events field|
+	},
+
+	'add/edit event start date' => {
+		message => q|Event Start Date|,
+		lastUpdated => 1138837472,
+		context => q|Event start date field label|
+	},
+
+	'add/edit event start date description' => {
+		message => q|The time and date when the event starts.|,
+		lastUpdated => 1131394072,
+		context => q|hover help for Event Start Date field|
+	},
+
+	'add/edit event end date' => {
+		message => q|Event End Date|,
+		lastUpdated => 1138837472,
+		context => q|Event end date field label|
+	},
+
+	'add/edit event start end description' => {
+		message => q|The time and date when the event ends.|,
+		lastUpdated => 1138837560,
+		context => q|hover help for Event End Date field|
 	},
 
 	'group to approve events' => {
@@ -77,6 +113,16 @@ our $I18N = { ##hashref of hashes
 
 	'add/edit event price description' => {
 		message => q|The cost to attend the event.|,
+		lastUpdated => 1138312761,
+	},
+
+	'add/edit global prerequisite' => {
+		message => q|Global Prerequisites|,
+		lastUpdated => 1138312761,
+	},
+
+	'add/edit global prerequisite description' => {
+		message => q|When set to yes, you may assign events belonging to another instance of an Event Management System Asset as a prerequisite event for one of the events defined in this instance os the asset.  When set to no, only events defined within this instance of the asset may be used as prerequisites.|,
 		lastUpdated => 1138312761,
 	},
 
