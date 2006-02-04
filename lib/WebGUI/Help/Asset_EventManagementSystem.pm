c934de3c84c9fa630542a5c0af3dd8710a0402f4
Colin Kuskie <colink@perldreamer.com>
2006-02-04 01:07:51 +0000

    beginning of help for EMS

---
+++ lib/WebGUI/Help/Asset_EventManagementSystem.pm
@@ -0,0 +1,42 @@
+package WebGUI::Help::Asset_EventManagementSystem; ## Be sure to change the package name to match your filename.
+
+##Stub document for creating help documents.
+
+our $HELP = {
+	'event management system add/edit' => {
+		title => 'add/edit help title',
+		body => 'add/edit help body',
+		fields => [
+                        {
+                                title => 'display template',
+                                description => 'display template description',
+                                namespace => 'Asset_EventManagementSystem',
+                        },
+                        {
+                                title => 'paginate after',
+                                description => 'paginate after description',
+                                namespace => 'Asset_EventManagementSystem',
+                        },
+                        {
+                                title => 'group to add events',
+                                description => 'group to add events description',
+                                namespace => 'Asset_EventManagementSystem',
+                        },
+                        {
+                                title => 'group to approve events',
+                                description => 'group to approve events description',
+                                namespace => 'Asset_EventManagementSystem',
+                        },
+                        {
+                                title => 'global prerequisite',
+                                description => 'global prerequisite description',
+                                namespace => 'Asset_EventManagementSystem',
+                        },
+		],
+		related => [
+		],
+	},
+
+};
+
+1;  ##All perl modules must return true
