e3b81ed09a5b177cc454af963ff30e09c7b1fb06
Matthew Wilson <matt@plainblack.com>
2006-02-11 17:53:08 +0000

    fix [ 1423324 ] Delete locked asset

--- lib/WebGUI/Asset.pm
+++ lib/WebGUI/Asset.pm
@@ -102,6 +102,7 @@ Unique hash identifier for a user. If not supplied, current user.
 sub canEdit {
 	my $self = shift;
 	my $userId = shift || $self->session->user->userId;
+ 	return 0 if ($self->isLocked && !$self->canEditIfLocked);
  	if ($userId eq $self->get("ownerUserId")) {
                 return 1;
 	}
