d3c13840f5555fdfa90e9962c55eed2268b95d27
Colin Kuskie <colink@perldreamer.com>
2006-02-26 22:43:34 +0000

    pull out debugging code for versions

--- sbin/upgrade.pl
+++ sbin/upgrade.pl
@@ -242,15 +242,12 @@ print "\nREADY TO BEGIN UPGRADES\n" unless ($quiet);
 my $notRun = 1;
 			
 chdir($upgradesPath);
-use Data::Dumper;
 foreach my $filename (keys %config) {
 	print "Considering $filename\n";
 	my $clicmd = $config{$filename}{mysqlCLI} || $mysql;
 	my $dumpcmd = $config{$filename}{mysqlDump} || $mysqldump;
 	my $backupTo = $config{$filename}{backupPath} || $backupDir;
 	mkdir($backupTo);
-	print Dumper \%config;
-	print Dumper \%upgrade;
 	print ("sql:".$upgrade{$config{$filename}{version}}{sql}."\n");
 	print ("pl:".$upgrade{$config{$filename}{version}}{pl}."\n");
 	while ($upgrade{$config{$filename}{version}}{sql} ne "" || $upgrade{$config{$filename}{version}}{pl} ne "") {
