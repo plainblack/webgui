fd1eb52e3ef9ddf19c548f3324f0639bc4209429
Colin Kuskie <colink@perldreamer.com>
2006-04-14 02:49:31 +0000

    Docs for AdSpace Macro

---
+++ lib/WebGUI/i18n/English/Macro_AdSpace.pm
@@ -0,0 +1,28 @@
+package WebGUI::i18n::English::Macro_AdSpace;
+
+our $I18N = {
+	'adspace title' => {
+		message => q|Ad Space Macro|,
+		lastUpdated => 1144979657,
+	},
+
+	'adspace body' => {
+		message => q|
+<p><b>&#94;AdSpace;</b><br>
+<b>&#94;AdSpace(adName);</b><br>
+This macro is used to display advertisements from the AdSpace system on a page.  The macro
+takes a single argument, wihch is the name of the Ad Space from the AdSpace manager to display.
+As a convenience, the AdSpace manager will display the macro code to put into a page to
+display each Ad Space.</p>
+|,
+		lastUpdated => 1144979672,
+	},
+
+	'macroName' => {
+		message => q|Ad Space|,
+		lastUpdated => 1144979669,
+	},
+
+};
+
+1;
