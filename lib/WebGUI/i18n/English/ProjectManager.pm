27fcef42f52c97b98de5d66b6ab1daa72419769a
Frank Dillon <frank@plainblack.com>
2006-05-08 13:47:47 +0000

    English International file for Project Management Asset

---
+++ lib/WebGUI/i18n/English/ProjectManager.pm
@@ -0,0 +1,377 @@
+package WebGUI::i18n::English::ProjectManager;
+
+our $I18N = {
+	'projectDashboardTemplate hoverhelp' => {
+		message => q|Template to use for main view of Project Management Application|,
+		lastUpdated => 0
+		},
+
+	'projectDashboardTemplate label' => {
+		message => q|Default Project Manager View Template|,
+		lastUpdated => 0
+		},
+    
+	'projectDisplayTemplate hoverhelp' => {
+		message => q|Template to use for displaying individual projects.  Default template displays Gantt chart representation of projects|,
+		lastUpdated => 0
+		},
+
+	'projectDisplayTemplate label' => {
+		message => q|Default Project Display Template|,
+		lastUpdated => 0
+		},
+	
+	'ganttChartTemplate hoverhelp' => {
+		message => q|Template to use for drawing the gantt chart within the table.|,
+		lastUpdated => 0
+		},
+
+	'ganttChartTemplate label' => {
+		message => q|Default Gantt Chart Template|,
+		lastUpdated => 0
+		},
+	
+	'editTaskTemplate hoverhelp' => {
+		message => q|Template to use for displaying the edit task dialog.|,
+		lastUpdated => 0
+		},
+
+	'editTaskTemplate label' => {
+		message => q|Default Edit Task Template|,
+		lastUpdated => 0
+		},
+				
+	'groupToAdd hoverhelp' => {
+		message => q|Group that is able to create projects|,
+		lastUpdated => 0
+		},
+
+	'groupToAdd label' => {
+		message => q|Group to Add Projects|,
+		lastUpdated => 0
+		},
+			
+	'assetName' => {
+		message => q|Project Management System|,
+		lastUpdated => 0
+		},
+		
+     'create project' => {
+		message => q|Create New Project|,
+		lastUpdated => 0
+		},
+
+	 'edit project' => {
+		message => q|Edit Project|,
+		lastUpdated => 0
+		},
+	 
+	 'project id' => {
+		message => q|Project Id|,
+		lastUpdated => 0
+		},
+
+	 'project name hoverhelp' => {
+		message => q|Enter the name of the project|,
+		lastUpdated => 0
+		},
+	 
+	 'project name label' => {
+		message => q|Project Name|,
+		lastUpdated => 0
+		},
+     
+	 'project description hoverhelp' => {
+		message => q|Enter a description for the project.|,
+		lastUpdated => 0
+		},
+
+	 'project description label' => {
+		message => q|Project Description|,
+		lastUpdated => 0
+		},
+	 
+	 'project manager hoverhelp' => {
+		message => q|Choose a group of users that are able to manage this project.  Project Managers will be able to edit project tasks|,
+		lastUpdated => 0
+		},
+	 
+	 'project manager label' => {
+		message => q|Project Managers Group|,
+		lastUpdated => 0
+		},
+		
+	 'hours per day hoverhelp' => {
+		message => q|Choose number of hours which generally represents a full day of work.  This will serve as a basis for estimating how many days a task takes.  This will be overridden if a resource is set to work more or less hours in a day, and that resource is attached to the task.|,
+		lastUpdated => 0
+		},
+	 
+	 'hours per day label' => {
+		message => q|Working Hours Per Day|,
+		lastUpdated => 0
+		},
+
+	 'duration units hoverhelp' => {
+		message => q|Choose the unit by which you wish to track a project.  This will determine how you will enter the time each task takes.|,
+		lastUpdated => 0
+		},
+	 
+	 'duration units label' => {
+		message => q|Track Project In|,
+		lastUpdated => 0
+		},
+     
+	 'target budget hoverhelp' => {
+		message => q|Enter the estimated cost amount the project should track to.  If your resources are properly configured, the system will track this target against actual costs.  Leave this value zero if you do not wish to track project costs|,
+		lastUpdated => 0
+		},
+
+	 'target budget label' => {
+		message => q|Project Cost Estimate|,
+		lastUpdated => 0
+		},
+	 
+	 'project new label' => {
+		message => q|New Project|,
+		lastUpdated => 0
+		},
+	 
+	 'project name label' => {
+		message => q|Project Name|,
+		lastUpdated => 0
+		},
+	 
+	 'project start date label' => {
+		message => q|Start|,
+		lastUpdated => 0
+		},
+	 
+	 'project end date label' => {
+		message => q|End|,
+		lastUpdated => 0
+		},
+	 
+	 'project cost label' => {
+		message => q|Estimated Cost|,
+		lastUpdated => 0
+		},
+	 
+	 'project complete label' => {
+		message => q|% Complete|,
+		lastUpdated => 0
+		},
+	 
+	 'project action label' => {
+		message => q|Actions|,
+		lastUpdated => 0
+		},
+	 
+	 'no projects' => {
+		message => q|Project List is Empty|,
+		lastUpdated => 0
+		},
+	 
+	 'project edit title' => {
+		message => q|Edit Project|,
+		lastUpdated => 0
+		},
+	 
+	 'project delete title' => {
+		message => q|Delete Project|,
+		lastUpdated => 0
+		},
+	 
+	 'project delete warning' => {
+		message => q|Are you sure you wish to delete this project and all of it's associated tasks?|,
+		lastUpdated => 0
+		},
+	 
+	 'project start task label' => {
+		message => q|Project Start|,
+		lastUpdated => 0
+		},
+		
+	  'hours label' => {
+		message => q|Hours|,
+		lastUpdated => 0
+		},
+	 
+	 'days label' => {
+		message => q|Days|,
+		lastUpdated => 0
+		},
+		
+	  'hours label abbrev' => {
+		message => q|hrs|,
+		lastUpdated => 0
+		},
+	 
+	 'days label abbrev' => {
+		message => q|days|,
+		lastUpdated => 0
+		},
+		
+	 'task name label' => {
+		message => q|Task|,
+		lastUpdated => 0
+		},
+		
+	 'task duration label' => {
+		message => q|Duration|,
+		lastUpdated => 0
+		},
+	 
+	 'task start label' => {
+		message => q|Start|,
+		lastUpdated => 0
+		},
+	 
+	 'task end label' => {
+		message => q|Finish|,
+		lastUpdated => 0
+		},
+	 
+	 'task dependant label' => {
+		message => q|Pred|,
+		lastUpdated => 0
+		},
+	 
+	 'add task label' => {
+		message => q|Add Task|,
+		lastUpdated => 0
+		},
+	 
+	 'edit task label' => {
+		message => q|Edit Task|,
+		lastUpdated => 0
+		},
+	 
+	 'task name error' => {
+		message => q|Task Name must be entered in order to save|,
+		lastUpdated => 0
+		},
+	 
+	 'task start error' => {
+		message => q|Start Date must be entered in order to save|,
+		lastUpdated => 0
+		},
+	 
+	 'task end error' => {
+		message => q|End Date must be entered in order to save|,
+		lastUpdated => 0
+		},
+	
+	 'task back label' => {
+		message => q|Back To Dashboard|,
+		lastUpdated => 0
+		},
+	 
+	 'task resources label' => {
+		message => q|Manage Resources|,
+		lastUpdated => 0
+		},
+	 
+	 'task greaterthan error' => {
+		message => q|End Date cannot be greater than Start Date|,
+		lastUpdated => 0
+		},
+	 
+	 'monday label' => {
+		message => q|M|,
+		lastUpdated => 0
+		},
+	 
+	 'tuesday label' => {
+		message => q|T|,
+		lastUpdated => 0
+		},
+	 
+	 'wednesday label' => {
+		message => q|W|,
+		lastUpdated => 0
+		},
+	
+	 'thursday label' => {
+		message => q|T|,
+		lastUpdated => 0
+		},
+	 
+	 'friday label' => {
+		message => q|F|,
+		lastUpdated => 0
+		},
+	 
+	 'saturday label' => {
+		message => q|S|,
+		lastUpdated => 0
+		},
+		
+	 'sunday label' => {
+		message => q|S|,
+		lastUpdated => 0
+		},
+	 
+	 'task previousPredecessor error' => {
+		message => q|Predecessor must be a previous task.|,
+		lastUpdated => 0
+		},
+	 
+	 'task samePredecessor error' => {
+		message => q|Task cannot be it's own predecessor|,
+		lastUpdated => 0
+		},
+	
+	 'task noPredecessor error' => {
+		message => q|Predecessor entered does not exist|,
+		lastUpdated => 0
+		},
+	 
+	 'task invalidMove error' => {
+		message => q|The start date that you have selected for this task is invalid as it's predecessor's end date will not be met.  Either remove the predecessor restriction from this task or change the end date of it's predecessor to make this date valid. |,
+		lastUpdated => 0
+        },
+	 
+	 'resource none' => {
+		message => q|No Resource|,
+		lastUpdated => 0
+		},
+		
+	 '' => {
+		message => q||,
+		lastUpdated => 0
+		},
+		
+	 '' => {
+		message => q||,
+		lastUpdated => 0
+		},
+		
+	 '' => {
+		message => q||,
+		lastUpdated => 0
+		},
+		
+	 '' => {
+		message => q||,
+		lastUpdated => 0
+		},
+		
+	 '' => {
+		message => q||,
+		lastUpdated => 0
+		},
+		
+	 '' => {
+		message => q||,
+		lastUpdated => 0
+		},
+		
+	 '' => {
+		message => q||,
+		lastUpdated => 0
+		},
+
+
+};
+
+1;
