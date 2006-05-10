139ff73b4960709a0e4a38ee5ccd9c8750a63177
Matthew Wilson <matt@plainblack.com>
2006-05-10 06:12:48 +0000

    prevent an infinite loop in weird cases.

--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -195,7 +195,7 @@ sub page {
 	if (defined($output) and $output eq "") {
 		if ($session->var->isAdminOn) { # they're expecting it to be there, so let's help them add it
 			my $asset = WebGUI::Asset->newByUrl($session, $session->url->getRefererUrl) || WebGUI::Asset->getDefault($session);
-			$session->http->setRedirect($asset->getUrl("func=add;class=WebGUI::Asset::Wobject::Layout;url=".$assetUrl));
+			$session->http->setRedirect($asset->getUrl("func=add;class=WebGUI::Asset::Wobject::Layout;url=".$assetUrl)) unless $session->url->getRequestedUrl eq $session->url->getRefererUrl;
 		} else { # not in admin mode, so can't create it,  so display not found
 			$session->http->setStatus("404","Page Not Found");
 			my $notFound = WebGUI::Asset->getNotFound($session);
