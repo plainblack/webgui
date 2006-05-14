b2def2113047379013b4a83882ceed431ba26caf
Frank Dillon <frank@plainblack.com>
2006-05-14 15:29:47 +0000

    Initial install of Time Tracker

---
+++ lib/WebGUI/i18n/English/Asset_TimeTracking.pm
@@ -0,0 +1,140 @@
+package WebGUI::i18n::English::Asset_TimeTracker;
+
+our $I18N = {
+	'userViewTemplate hoverhelp' => {
+		message => q|Template to use for main user view of Time Tracker Application|,
+		lastUpdated => 0
+		},
+
+	'userViewTemplate label' => {
+		message => q|Default Time Tracker User View Template|,
+		lastUpdated => 0
+		},
+
+	'managerViewTemplate hoverhelp' => {
+		message => q|Template to use for manager view of Time Tracker Application|,
+		lastUpdated => 0
+		},
+
+	'managerViewTemplate label' => {
+		message => q|Default Time Tracker Manager View Template|,
+		lastUpdated => 0
+		},
+
+	'groupToManage hoverhelp' => {
+		message => q|Group that is able to view all time sheets and update completed time records|,
+		lastUpdated => 0
+		},
+
+	'groupToManage label' => {
+		message => q|Group to Manage Time|,
+		lastUpdated => 0
+		},
+    
+	'timeRowTemplateId hoverHelp' => {
+		message => q|Template to user for building rows in the time tracker|,
+		lastUpdated => 0
+		},			
+	
+	'timeRowTemplateId label' => {
+		message => q|Default Time Tracker Row|,
+		lastUpdated => 0
+		},
+
+	'assetName' => {
+		message => q|Time Tracking System|,
+		lastUpdated => 0
+		},
+		
+	'time report header' => {
+		message => q|Time entry for week ending %s|,
+		lastUpdated => 0
+		},
+
+	'total hours label' => {
+		message => q|Total Hours|,
+		lastUpdated => 0
+		},
+	
+	'time report date label' => {
+		message => q|Date|,
+		lastUpdated => 0
+		},
+		
+	'time report project label' => {
+		message => q|Project|,
+		lastUpdated => 0
+		},			
+	
+	'time report task label' => {
+		message => q|Task|,
+		lastUpdated => 0
+		},
+
+	'time report hours label' => {
+		message => q|Hours|,
+		lastUpdated => 0
+		},
+	
+	'time report comments label' => {
+		message => q|Comments|,
+		lastUpdated => 0
+		},
+		
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},
+	
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},
+		
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},			
+	
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},
+
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},
+	
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},
+		
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},			
+	
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},
+
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},
+	
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},
+		
+	'' => {
+		message => q||,
+		lastUpdated => 0
+		},				
+};
+
+1;
