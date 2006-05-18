ad84dc5556649fc21e522a95b1afb58e738f433d
JT Smith <jt@plainblack.com>
2006-05-18 14:59:45 +0000

    preparing for 6.99.2 bugfix cycle

---
+++ docs/upgrades/upgrade_6.99.1-6.99.2.pl
@@ -0,0 +1,62 @@
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2006 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+use lib "../../lib";
+use strict;
+use Getopt::Long;
+use WebGUI::Session;
+
+
+my $toVersion = "0.0.0"; # make this match what version you're going to
+my $quiet; # this line required
+
+
+my $session = start(); # this line required
+
+# upgrade functions go here
+
+finish($session); # this line required
+
+
+##-------------------------------------------------
+#sub exampleFunction {
+#	my $session = shift;
+#	print "\tWe're doing some stuff here that you should know about.\n" unless ($quiet);
+#	# and here's our code
+#}
+
+
+
+# ---- DO NOT EDIT BELOW THIS LINE ----
+
+#-------------------------------------------------
+sub start {
+	my $configFile;
+	$|=1; #disable output buffering
+	GetOptions(
+    		'configFile=s'=>\$configFile,
+        	'quiet'=>\$quiet
+	);
+	my $session = WebGUI::Session->open("../..",$configFile);
+	$session->user({userId=>3});
+	my $versionTag = WebGUI::VersionTag->getWorking($session);
+	$versionTag->set({name=>"Upgrade to ".$toVersion});
+	$session->db->write("insert into webguiVersion values (".$session->db->quote($toVersion).",'upgrade',".$session->datetime->time().")");
+	return $session;
+}
+
+#-------------------------------------------------
+sub finish {
+	my $session = shift;
+	my $versionTag = WebGUI::VersionTag->getWorking($session);
+	$versionTag->commit;
+	$session->close();
+}
+
