58566a6ed604fae0dc9e83186f8bc5c38cd4d259
Colin Kuskie <colink@perldreamer.com>
2006-06-10 17:13:16 +0000

    template variable breakout

--- lib/WebGUI/Help/Asset_Matrix.pm
+++ lib/WebGUI/Help/Asset_Matrix.pm
@@ -223,6 +223,72 @@ our $HELP = {
 	'compare template' => {
 		title => 'comparison template help title',
 		body => 'comparison template help body',
+		variables => [
+		          {
+		            'name' => 'isTooMany'
+		          },
+		          {
+		            'name' => 'isTooFew'
+		          },
+		          {
+		            'name' => 'compare.form'
+		          },
+		          {
+		            'name' => 'product_loop',
+		            'variables' => [
+		                             {
+		                               'name' => 'name'
+		                             },
+		                             {
+		                               'name' => 'version'
+		                             },
+		                             {
+		                               'name' => 'url',
+		                               'description' => 'details url'
+		                             }
+		                           ]
+		          },
+		          {
+		            'name' => 'lastupdated_loop',
+		            'variables' => [
+		                             {
+		                               'name' => 'lastUpdated'
+		                             }
+		                           ]
+		          },
+		          {
+		            'name' => 'category_loop',
+		            'variables' => [
+		                             {
+		                               'name' => 'category',
+		                               'description' => 'tmplVar category'
+		                             },
+		                             {
+		                               'name' => 'columnCount'
+		                             },
+		                             {
+		                               'name' => 'row_loop',
+		                               'variables' => [
+		                                                {
+		                                                  'name' => 'column_loop',
+		                                                  'variables' => [
+		                                                                   {
+		                                                                     'name' => 'value'
+		                                                                   },
+		                                                                   {
+		                                                                     'name' => 'class'
+		                                                                   },
+		                                                                   {
+		                                                                     'name' => 'description',
+		                                                                     'description' => 'tmplVar field.description'
+		                                                                   }
+		                                                                 ]
+		                                                }
+		                                              ]
+		                             }
+		                           ]
+		          }
+		],
 		related => [
 			{
 				tag => 'matrix add/edit',
@@ -275,6 +341,159 @@ our $HELP = {
 	'main template' => {
 		title => 'matrix template help title',
 		body => 'matrix template help body',
+		variables => [
+		          {
+		            'name' => 'compare.form',
+		          },
+		          {
+		            'name' => 'search.url'
+		          },
+		          {
+		            'name' => 'isLoggedIn'
+		          },
+		          {
+		            'name' => 'field.list.url'
+		          },
+		          {
+		            'name' => 'listing.add.url'
+		          },
+		          {
+		            'name' => 'best.views.url'
+		          },
+		          {
+		            'name' => 'best.views.count'
+		          },
+		          {
+		            'name' => 'best.views.name'
+		          },
+		          {
+		            'name' => 'best.compares.url'
+		          },
+		          {
+		            'name' => 'best.compares.count'
+		          },
+		          {
+		            'name' => 'best.compares.name'
+		          },
+		          {
+		            'name' => 'best.clicks.url'
+		          },
+		          {
+		            'name' => 'best.clicks.count'
+		          },
+		          {
+		            'name' => 'best.clicks.name'
+		          },
+		          {
+		            'name' => 'best_rating_loop',
+		            'variables' => [
+		                             {
+		                               'name' => 'url',
+		                               'description' => 'tmplVar best.url'
+		                             },
+		                             {
+		                               'name' => 'category',
+		                               'description' => 'tmplVar best.category'
+		                             },
+		                             {
+		                               'name' => 'name',
+		                               'description' => 'tmplVar best.name'
+		                             },
+		                             {
+		                               'name' => 'mean'
+		                             },
+		                             {
+		                               'name' => 'median'
+		                             },
+		                             {
+		                               'name' => 'count'
+		                             }
+		                           ]
+		          },
+		          {
+		            'name' => 'worst_rating_loop',
+		            'variables' => [
+		                             {
+		                               'name' => 'url',
+		                               'description' => 'tmplVar worst.url'
+		                             },
+		                             {
+		                               'name' => 'category',
+		                               'description' => 'tmplVar worst.category'
+		                             },
+		                             {
+		                               'name' => 'name',
+		                               'description' => 'tmplVar worst.name'
+		                             },
+		                             {
+		                               'name' => 'mean',
+		                               'description' => 'tmplVar worst.mean'
+		                             },
+		                             {
+		                               'name' => 'median',
+		                               'description' => 'tmplVar worst.median'
+		                             },
+		                             {
+		                               'name' => 'count',
+		                               'description' => 'tmplVar worst.count'
+		                             }
+		                           ]
+		          },
+		          {
+		            'name' => 'ratings.details.url'
+		          },
+		          {
+		            'name' => 'best.posts.url'
+		          },
+		          {
+		            'name' => 'best.updated.url'
+		          },
+		          {
+		            'name' => 'best.updated.date'
+		          },
+		          {
+		            'name' => 'best.updated.name'
+		          },
+		          {
+		            'name' => 'last_update_loop',
+		            'variables' => [
+		                             {
+		                               'name' => 'url',
+		                               'description' => 'tmplVar last.url'
+		                             },
+		                             {
+		                               'name' => 'name',
+		                               'description' => 'tmplVar last.name'
+		                             },
+		                             {
+		                               'name' => 'lastUpdated',
+		                               'description' => 'tmplVar last.lastUpdated'
+		                             }
+		                           ]
+		          },
+		          {
+		            'name' => 'user.count'
+		          },
+		          {
+		            'name' => 'current.user.count'
+		          },
+		          {
+		            'name' => 'listing.count'
+		          },
+		          {
+		            'name' => 'pending_list',
+		            'variables' => [
+		                             {
+		                               'name' => 'url',
+		                               'description' => 'tmplVar pending.url'
+		                             },
+		                             {
+		                               'name' => 'productName',
+		                               'description' => 'tmplVar pending.productName'
+		                             }
+		                           ]
+		          }
+		        ],
 		related => [
 			{
 				tag => 'matrix add/edit',
@@ -301,6 +520,115 @@ our $HELP = {
 	'listing detail template' => {
 		title => 'detail template help title',
 		body => 'detail template help body',
+		variables => [
+		          {
+		            'name' => 'discussion'
+		          },
+		          {
+		            'name' => 'screenshot',
+		            'description' => 'tmplVar screenshot'
+		          },
+		          {
+		            'name' => 'thumbnail'
+		          },
+		          {
+		            'name' => 'email.form'
+		          },
+		          {
+		            'name' => 'email.wasSent'
+		          },
+		          {
+		            'name' => 'edit.url'
+		          },
+		          {
+		            'name' => 'user.canEdit'
+		          },
+		          {
+		            'name' => 'user.canApprove'
+		          },
+		          {
+		            'name' => 'approve.url'
+		          },
+		          {
+		            'name' => 'delete.url'
+		          },
+		          {
+		            'name' => 'isPending'
+		          },
+		          {
+		            'name' => 'lastUpdated.epoch'
+		          },
+		          {
+		            'name' => 'lastUpdated.date'
+		          },
+		          {
+		            'name' => 'id'
+		          },
+		          {
+		            'name' => 'description',
+		            'description' => 'listing description'
+		          },
+		          {
+		            'name' => 'productName'
+		          },
+		          {
+		            'name' => 'productUrl'
+		          },
+		          {
+		            'name' => 'productUrl.click'
+		          },
+		          {
+		            'name' => 'manufacturerName'
+		          },
+		          {
+		            'name' => 'manufacturerUrl'
+		          },
+		          {
+		            'name' => 'manufacturerUrl.click'
+		          },
+		          {
+		            'name' => 'versionNumber'
+		          },
+		          {
+		            'name' => 'views'
+		          },
+		          {
+		            'name' => 'compares'
+		          },
+		          {
+		            'name' => 'clicks'
+		          },
+		          {
+		            'name' => 'ratings'
+		          },
+		          {
+		            'name' => 'CATEGORY_NAME_loop',
+		            'variables' => [
+		                             {
+		                               'name' => 'value',
+		                             },
+		                             {
+		                               'name' => 'name',
+		                               'description' => 'tmplVar name'
+		                             },
+		                             {
+		                               'name' => 'label'
+		                             },
+		                             {
+		                               'name' => 'description',
+		                               'description' => 'listing description'
+		                             },
+		                             {
+		                               'name' => 'category',
+		                               'description' => 'tmplVar category'
+		                             },
+		                             {
+		                               'name' => 'class',
+		                               'description' => 'tmplVar class'
+		                             }
+		                           ]
+		          }
+		        ],
 		related => [
 			{
 				tag => 'matrix add/edit',
