785645b2b6ef8887fc0316f3f265fad12f87a181
Frank Dillon <frank@plainblack.com>
2006-06-30 19:59:44 +0000

    fix bug which doesn't allow all macros to be processed

--- www/extras/tinymce2/jscripts/webgui.js
+++ www/extras/tinymce2/jscripts/webgui.js
@@ -2,7 +2,7 @@
 
 function tinyMCE_WebGUI_URLConvertor(url, node, on_save) {
 	// The next line would have tried formatting the URL, but we don't want it to
-        url = tinyMCE.convertURL(url, node, on_save);
+        //url = tinyMCE.convertURL(url, node, on_save);
         // Do custom WebUI convertion, replace back ^();
         url = url.replace(new RegExp("%5E", "g"), "^");
         url = url.replace(new RegExp("%3B", "g"), ";");
