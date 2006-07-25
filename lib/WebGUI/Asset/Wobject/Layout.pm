03e602363ba0268991a01c0438ab89e64e0a80bf
Wouter van Oijen <wouter@webgui-svn>
2006-07-25 12:34:08 +0000

    XHTML compliance

--- lib/WebGUI/Asset/Wobject/Layout.pm
+++ lib/WebGUI/Asset/Wobject/Layout.pm
@@ -222,7 +222,7 @@ sub view {
 	if ($self->{_viewVars}{showAdmin} && $self->canEditIfLocked) {
 		# under normal circumstances we don't put HTML stuff in our code, but this will make it much easier
 		# for end users to work with our templates
-		$self->{_viewVars}{"dragger.icon"} = '<div id="dragTrigger" class="dragTrigger">'.$self->session->icon->drag('class="dragTrigger"').'</div></div>';
+		$self->{_viewVars}{"dragger.icon"} = '<div id="dragTrigger" class="dragTrigger">'.$self->session->icon->drag('class="dragTrigger"').'</div>';
 		$self->{_viewVars}{"dragger.init"} = '
 			<iframe id="dragSubmitter" style="display: none;" src="'.$self->session->url->extras('spacer.gif').'"></iframe>
 			<script type="text/javascript">
