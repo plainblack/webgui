a7f5942e56ff58e5106bfc2691389f9dc9f4a59c
Colin Kuskie <colink@perldreamer.com>
2006-11-06 19:04:52 +0000

    added missing docs for new workflow activity

---
+++ lib/WebGUI/Help/Workflow_Activity_DeleteExportedFiles.pm
@@ -0,0 +1,23 @@
+package WebGUI::Help::Workflow_Activity_DeleteExportedFiles;
+
+our $HELP = {
+	'delete exported files' => {
+		title => 'activityName',
+		body => 'delete exported files body',
+		isa => [
+			{
+				namespace => "Workflow_Activity",
+				tag => "add/edit workflow activity"
+			},
+		],
+		fields => [
+		],
+		variables => [
+		],
+		related => [
+		],
+	},
+
+};
+
+1;  ##All perl modules must return true
--- lib/WebGUI/i18n/English/Workflow_Activity_DeleteExportedFiles.pm
+++ lib/WebGUI/i18n/English/Workflow_Activity_DeleteExportedFiles.pm
@@ -4,8 +4,14 @@ our $I18N = {
 	'activityName' => {
 		message => q|Delete Exported Files|,
 		context => q|The name of this workflow activity.|,
-		lastUpdated => 1162238122,
+		lastUpdated => 1162839802,
 	},
+
+	'delete exported files body' => {
+		message => q|<p>Deletes an asset's exported files.</p>|,
+		lastUpdated => 1162839804,
+	},
+
 };
 
 1;
