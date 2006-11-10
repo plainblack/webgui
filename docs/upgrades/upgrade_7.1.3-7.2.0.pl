e982302c2af7c1eb472ec5d5331e1b87094be83a
Drake <drake@plainblack.com>
2006-11-10 18:29:37 +0000

    Update changelog and gotchas, and hopefully final update to r3226 bugfixes in 7.2.0 upgrade script.

--- docs/upgrades/upgrade_7.1.3-7.2.0.pl
+++ docs/upgrades/upgrade_7.1.3-7.2.0.pl
@@ -205,10 +205,10 @@ EOT
 		# Blah, some duplication with RSSCapable.pm.
 		my ($revisionDate) = $session->db->quickArray("SELECT MAX(revisionDate) FROM Collaboration WHERE assetId = ?", [$csId]);
 		my $cs = WebGUI::Asset->newByDynamicClass($session, $csId, $revisionDate);
-		if ($cs->isPrototype or not($cs->get('status') eq 'approved' or
-					    $cs->get('status') eq 'archived')) {
+		if ($cs->get('isPrototype') or not($cs->get('status') eq 'approved' or
+						   $cs->get('status') eq 'archived')) {
 			$cs->update({ rssCapableRssEnabled => 1, rssCapableRssFromParentId => undef });
-			if (!$cs->isPrototype) {
+			if (!$cs->get('isPrototype')) {
 				# Update the most recent published one too.
 				my $csp = WebGUI::Asset->newByDynamicClass($session, $csId);
 				if ($csp) {
