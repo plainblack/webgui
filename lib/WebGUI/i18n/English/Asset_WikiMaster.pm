d1d368dcd1290e977ebe2f53fa069984cacc7217
JT Smith <jt@plainblack.com>
2006-11-27 20:04:53 +0000

    wiki

---
+++ lib/WebGUI/i18n/English/Asset_WikiMaster.pm
@@ -0,0 +1,94 @@
+package WebGUI::i18n::English::Asset_WikiMaster;
+
+our $I18N =
+{
+ 'assetName' =>
+ { lastUpdated => 1160157064, message => 'Wiki' },
+
+ 'groupToEditPages hoverHelp' =>
+ { lastUpdated => 1160157064, message => q|Choose a group of users who will be able to edit pages in this wiki instance.  They will not, by default, be able to delete pages or revisions, or edit protected pages.| },
+ 'groupToEditPages label' =>
+ { lastUpdated => 1160157064, message => q|Who can edit pages?| },
+
+ 'groupToAdminister hoverHelp' =>
+ { lastUpdated => 1160157064, message => q|Choose a group of users who will be able to perform administrative actions on pages in this wiki instance; such actions include deletion of pages and page revisions, and protecting and unprotecting of pages.| },
+ 'groupToAdminister label' =>
+ { lastUpdated => 1160157064, message => q|Who can administer?| },
+
+ 'richEditor hoverHelp' =>
+ { lastUpdated => 1160157064, message => q|Which rich editor to use for editing pages in this wiki instance.| },
+ 'richEditor label' =>
+ { lastUpdated => 1160157064, message => q|Rich Editor| },
+
+ 'pageTemplateId hoverHelp' =>
+ { lastUpdated => 1160157064, message => q|Which template to use to display pages.| },
+ 'pageTemplateId label' =>
+ { lastUpdated => 1160157064, message => q|Page Template| },
+
+ 'frontPageTemplateId hoverHelp' =>
+ { lastUpdated => 1161031607, message => q|Which template to use for the front page.| },
+ 'frontPageTemplateId label' =>
+ { lastUpdated => 1161031607, message => q|Front Page Template| },
+
+ 'recentChangesTemplateId hoverHelp' =>
+ { lastUpdated => 1160157064, message => q|Which template to use for the recent changes display.| },
+ 'recentChangesTemplateId label' =>
+ { lastUpdated => 1160157064, message => q|Recent Changes Template| },
+
+ 'pageHistoryTemplateId hoverHelp' =>
+ { lastUpdated => 1160505291, message => q|Which template to use for the page history display.| },
+ 'pageHistoryTemplateId label' =>
+ { lastUpdated => 1160505291, message => q|Page History Template| },
+
+ 'pageListTemplateId hoverHelp' =>
+ { lastUpdated => 1160417517, message => q|Which template to use for displaying lists of pages.| },
+ 'pageListTemplateId label' =>
+ { lastUpdated => 1160417517, message => q|Page List Template| },
+
+ 'searchTemplateId hoverHelp' =>
+ { lastUpdated => 1161031607, message => q|Which template to use for search results.| },
+ 'searchTemplateId label' =>
+ { lastUpdated => 1161031607, message => q|Search Template| },
+
+ 'recentChangesCount hoverHelp' =>
+ { lastUpdated => 1161031607, message => q|Maximum number of changes to display on the recent changes page.| },
+ 'recentChangesCount label' =>
+ { lastUpdated => 1161031607, message => q|Recent Changes Count| },
+ 'recentChangesCountFront hoverHelp' =>
+ { lastUpdated => 1161031607, message => q|Maximum number of changes to display on the front page.| },
+ 'recentChangesCountFront label' =>
+ { lastUpdated => 1161031607, message => q|Front Page Recent Changes Count| },
+
+ 'func addPage link text' =>
+ { lastUpdated => 1160157064, message => q|Add a new page| },
+ 'func listPages link text' =>
+ { lastUpdated => 1160417517, message => q|List all pages| },
+ 'func recentChanges link text' =>
+ { lastUpdated => 1160768887, message => q|Show recent changes| },
+ 'func view link text' =>
+ { lastUpdated => 1161031607, message => q|Back to front page| },
+ 'func search link text' =>
+ { lastUpdated => 1161118304, message => q|Search within pages| },
+ 'listPages title' =>
+ { lastUpdated => 1160417517, message => q|List of pages| },
+
+ 'actionN edited' =>
+ { lastUpdated => 1160505291, message => q|Edited| },
+ 'actionN trashed' =>
+ { lastUpdated => 1160505291, message => q|Deleted| },
+ 'actionN protected' =>
+ { lastUpdated => 1160505291, message => q|Protected| },
+ 'actionN unprotected' =>
+ { lastUpdated => 1160505291, message => q|Unprotected| },
+ 'actionN created' =>
+ { lastUpdated => 1160505291, message => q|Created| },
+
+ 'recentChanges title' =>
+ { lastUpdated => 1161116593, message => q|Recent changes| },
+ 'search submit' =>
+ { lastUpdated => 1161031607, message => q|Search| },
+ 'search title' =>
+ { lastUpdated => 1161031607, message => q|Search| },
+};
+
+1;
---
+++ lib/WebGUI/i18n/English/Asset_WikiPage.pm
@@ -0,0 +1,31 @@
+package WebGUI::i18n::English::Asset_WikiPage;
+
+our $I18N =
+{
+ 'assetName' =>
+ { lastUpdated => 1160157064, message => 'Wiki Page' },
+
+ 'func edit link text' =>
+ { lastUpdated => 1160157064, message => q|Edit this page| },
+ 'func view link text' =>
+ { lastUpdated => 1160157064, message => q|View this page| },
+ 'func pageHistory link text' =>
+ { lastUpdated => 1160425002, message => q|View this page's history| },
+ 'func protect link text' =>
+ { lastUpdated => 1160425002, message => q|Protect this page| },
+ 'func unprotect link text' =>
+ { lastUpdated => 1160425002, message => q|Unprotect this page| },
+ 'func delete link text' =>
+ { lastUpdated => 1161121377, message => q|Delete this page| },
+ 'func wikiPurgeRevision link text' =>
+ { lastUpdated => 1161121377, message => q|Delete this revision of this page| },
+ 'func delete link confirm' =>
+ { lastUpdated => 1161121377, message => q|Are you sure you want to delete this page?  Deletions cannot be stored within version tags and take place immediately.| },
+ 'func wikiPurgeRevision link confirm' =>
+ { lastUpdated => 1161121377, message => q|Are you sure you want to delete this revision of this page?  The revision will be permanently erased.| },
+
+ 'pageHistory title' =>
+ { lastUpdated => 1161113157, message => q|History of "%s"| },
+};
+
+1;
