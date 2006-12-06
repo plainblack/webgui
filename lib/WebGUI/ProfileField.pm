e795f06fd88b99461585fda7ac7742329e073302
Drake <drake@plainblack.com>
2006-12-06 17:49:22 +0000

    Rollback r3340 since it broke specialState:init.

--- lib/WebGUI/ProfileField.pm
+++ lib/WebGUI/ProfileField.pm
@@ -107,14 +107,6 @@ sub create {
         my $self = $class->new($session,$id);
 	$self->setCategory($categoryId);
         $self->set($properties);
-
-	# Since we're creating a new profile field, give every user the default value for it.
-	my $default = WebGUI::Operation::Shared::secureEval($session,$properties->{dataDefault});
-	foreach my $userId ($session->db->buildArray("SELECT userId FROM users")) {
-		my $user = WebGUI::User->new($session, $userId);
-		$user->profileField($id, $default);
-	}
-
         return $self;
 }
 
@@ -191,7 +183,7 @@ sub formField {
 	} else {
 		$default = WebGUI::Operation::Shared::secureEval($self->session,$properties->{dataDefault});
 	}
-	$properties->{value} = $default unless defined $properties->{value};
+	$properties->{value} = $default;
 	if ($withWrapper == 1) {
 		return WebGUI::Form::DynamicField->new($self->session,%{$properties})->displayFormWithWrapper;
 	} elsif ($withWrapper == 2) {
