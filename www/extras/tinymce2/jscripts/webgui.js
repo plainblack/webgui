a2cf1ce813e36d3cbec10aa6cc8dc14687341e65
JT Smith <jt@plainblack.com>
2007-03-08 00:29:24 +0000

    - fix: Linking Pages

--- www/extras/tinymce2/jscripts/webgui.js
+++ www/extras/tinymce2/jscripts/webgui.js
@@ -3,14 +3,16 @@
 function tinyMCE_WebGUI_URLConvertor(url, node, on_save) {
         url = tinyMCE.convertURL(url, node, on_save);
         // Do custom WebGUI convertion, replace back ^();
+
+	// turn escaped macro characters back into the real thing
         url = url.replace(new RegExp("%5E", "g"), "^");
         url = url.replace(new RegExp("%3B", "g"), ";");
         url = url.replace(new RegExp("%28", "g"), "(");
         url = url.replace(new RegExp("%29", "g"), ")");
-	url = url.replace(/^\/\^/,"^");
-	url = url.replace(/http:\/\/\//,"/");
-	url = url.replace(/^.*(\^\/\;.*)$/,"$1");
-	url = url.replace(/^.*(\^FileUrl\(.*\)\;.*)$/,"$1");
+
+	// if there is a macro in the line, remove everythiing in front of the macro
+	url = url.replace(/^.*(\^.*)$/,"$1");
+
         return url;
 }
 
