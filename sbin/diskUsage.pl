ee9908768d8ee410c28a81ada0b3a0759805174d
Colin Kuskie <colink@perldreamer.com>
2007-05-15 20:33:21 +0000

    fix shebang line in diskUsage.pl and added license notice

--- sbin/diskUsage.pl
+++ sbin/diskUsage.pl
@@ -1,4 +1,12 @@
-#!/data/wre/prereqs/perl/bin/perl
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2006 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
 
 use lib "../lib";
 use Getopt::Long;
