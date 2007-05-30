261cd940c78f36fd4239cf2263cc0ad1123a7125
Colin Kuskie <colink@perldreamer.com>
2007-05-30 21:31:39 +0000

    stub out FormValidator so WebGUI::Session will compile

---
+++ lib/WebGUI/FormValidator.pm
@@ -0,0 +1,20 @@
+package WebGUI::FormValidator;
+
+=head1 LEGAL
+
+ -------------------------------------------------------------------
+  WebGUI is Copyright 2001-2007 Plain Black Corporation.
+ -------------------------------------------------------------------
+  Please read the legal notices (docs/legal.txt) and the license
+  (docs/license.txt) that came with this distribution before using
+  this software.
+ -------------------------------------------------------------------
+  http://www.plainblack.com                     info@plainblack.com
+ -------------------------------------------------------------------
+
+=cut
+
+use strict;
+
+1;
+
