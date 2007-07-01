a7dde82421f13e9f6c4c7b425b71cc6b75a4bccd
JT Smith <jt@plainblack.com>
2007-07-01 16:09:42 +0000

    updated copyright

--- lib/WebGUI/Asset/Wobject/Collaboration.pm
+++ lib/WebGUI/Asset/Wobject/Collaboration.pm
@@ -1,7 +1,7 @@
 package WebGUI::Asset::Wobject::Collaboration;
 
 #-------------------------------------------------------------------
-# WebGUI is Copyright 2001-2006 Plain Black Corporation.
+# WebGUI is Copyright 2001-2007 Plain Black Corporation.
 #-------------------------------------------------------------------
 # Please read the legal notices (docs/legal.txt) and the license
 # (docs/license.txt) that came with this distribution before using
