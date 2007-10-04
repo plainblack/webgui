f170f246c63a7aabce11ae0b75c12ef1625ba797
Graham Knop <graham@plainblack.com>
2007-10-04 22:21:11 +0000

    nevermind, that's an RFE and needs more work

--- lib/WebGUI/Asset/Wobject/SQLForm.pm
+++ lib/WebGUI/Asset/Wobject/SQLForm.pm
@@ -425,11 +425,6 @@ should be implemented in WebGUI::Form::myELement.
 		searchElement		=> 'selectList',
 		type			=> 'list',
 		},
-	selectBox 	=> {
-		name			=> 'Select box',
-		hasOptions		=> 1,
-        searchElement   => 'selectBox',
-    },
 	radioList 	=> {
 		name			=> 'Radio list',
 		hasOptions		=> 1,
