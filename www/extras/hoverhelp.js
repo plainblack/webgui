de13432ee8eab20ff515a7bf9b9564ad5af7a26f
Graham Knop <graham@plainblack.com>
2007-11-01 14:04:40 +0000

    more matrix hoverhelp fixes

--- www/extras/hoverhelp.js
+++ www/extras/hoverhelp.js
@@ -1,7 +1,10 @@
+var hoverHelpLoaded;
 YAHOO.util.Event.onDOMReady(function () {
+    if (hoverHelpLoaded)
+        return;
+    hoverHelpLoaded = true;
     var tips = YAHOO.util.Dom.getElementsByClassName('wg-hoverhelp');
-    var i;
-    for (i = 0; i < tips.length; i++) {
+    for (var i = tips.length; i--; ) {
         var myTip = new YAHOO.widget.Tooltip(tips[i], {  
             autodismissdelay: 1000000,
             context: tips[i].parentNode
