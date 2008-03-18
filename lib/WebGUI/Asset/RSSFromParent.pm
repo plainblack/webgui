b9134bcadbf3453d3f40ba342df13ba036b61a4a
Graham Knop <graham@plainblack.com>
2008-03-18 14:48:40 +0000

    Don't need to convert data to UTF8 anymore, it comes from mysql that way

--- lib/WebGUI/Asset/RSSFromParent.pm
+++ lib/WebGUI/Asset/RSSFromParent.pm
@@ -15,7 +15,6 @@ package WebGUI::Asset::RSSFromParent;
 =cut
 
 use strict;
-use Encode;
 use HTML::Entities;
 use Tie::IxHash;
 use base 'WebGUI::Asset';
@@ -70,7 +69,6 @@ sub update {
 sub _escapeXml {
 	my $text = shift;
     return $text unless (ref $text eq "");
-    $text = decode('utf8', $text);
     return HTML::Entities::encode_numeric($text)
 }
 
