d05233dc23855043dfe5aa558d388f0b7d4c6db8
Graham Knop <graham@plainblack.com>
2008-04-01 18:59:00 +0000

    fixed: Event related links truncated on dashes

--- lib/WebGUI/Asset/Event.pm
+++ lib/WebGUI/Asset/Event.pm
@@ -1155,7 +1155,7 @@ sub getRelatedLinks {
     my @links;
     my %found_links;
     # This is a regex stolen from Regexp::Common to match urls
-    for my $link ($self->get("relatedLinks") =~ m{\w+://[a-zA-Z0-9\\-_.!~*'():\@&=+\$,/?]+}g) {
+    for my $link ($self->get("relatedLinks") =~ m{\w+://[a-zA-Z0-9\-_.!~*'():\@&=+\$,/?]+}g) {
         push @links, $link
             unless $found_links{$link};
         $found_links{$link}++;
