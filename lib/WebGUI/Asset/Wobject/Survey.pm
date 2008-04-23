18b00e0a2ffaddc52d2acd22482c00f8b1a411e4
Kaleb Murphy <perlmonkey2@webgui-svn>
2008-04-23 18:11:07 +0000

    4 scales moved back to MC

--- lib/WebGUI/Asset/Wobject/Survey.pm
+++ lib/WebGUI/Asset/Wobject/Survey.pm
@@ -975,9 +975,9 @@ $self->session->errorHandler->warn("-------SurveyEnd $url");
 #sends the processed template and questions structure to the client
 sub showQuestions{
     my ($self,$section,$questions) = @_;
-    my %multipleChoice = ('Multiple Choice',1,'Gender',1,'Yes/No',1,'True/False',1);
+    my %multipleChoice = ('Multiple Choice',1,'Gender',1,'Yes/No',1,'True/False',1'Ideology',1, 'Race',1,'Party',1,'Education',1);
     my %scale = ('Agree/Disagree',1,'Oppose/Support',1,'Importance',1, 'Likelihood',1,'Certainty',1,'Satisfaction',1,'Confidence',1,
-        'Effectiveness',1,'Concern',1,'Risk',1,'Threat',1,'Security',1,'Ideology',1, 'Race',1,'Party',1,'Education',1);
+        'Effectiveness',1,'Concern',1,'Risk',1,'Threat',1,'Security',1);
     my %text = ('Text',1, 'Email',1, 'Phone Number',1, 'Text Date',1, 'Currency',1);
     my %slider = ('Slider',1, 'Dual Slider - Range',1, 'Multi Slider - Allocate',1);
     my %dateType = ('Date',1,'Date Range',1);
