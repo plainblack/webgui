26171e97b40a4a5e9fdd2a663e27e05f6d8a86c9
Ernesto Hernández-Novich <ernesto@webgui-svn>
2008-06-09 15:06:05 +0000

    Added missing copyright notice to utility scripts.

--- sbin/migrateCollabToGallery.pl
+++ sbin/migrateCollabToGallery.pl
@@ -1,3 +1,12 @@
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2008 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
 
 use lib "../lib";
 use strict;
