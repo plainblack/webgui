fcf53d7bde2a822c142134e8dd89fcd105b374fa
Colin Kuskie <colink@perldreamer.com>
2008-06-23 22:01:34 +0000

    add missing template variable help for the MiniCart macro

---
+++ lib/WebGUI/i18n/English/Macro_MiniCart.pm
@@ -0,0 +1,57 @@
+package WebGUI::i18n::English::Macro_MiniCart;
+
+use strict;
+
+our $I18N = {
+
+    'minicart title' => {
+        message => q|MiniCart Macro Template Variables|,
+        lastUpdated => 0,
+        context => q|Title for the MiniCart template variables page|
+    },
+
+    'items' => {
+        message => q|A loop containing some information about all items currently in the cart.|,
+        lastUpdated => 0,
+        context => q|Template variable.|
+    },
+
+    'name' => {
+        message => q|The configured title for the item.|,
+        lastUpdated => 0,
+        context => q|Template variable.|
+    },
+
+    'quantity' => {
+        message => q|The number of this item that are in the cart.|,
+        lastUpdated => 0,
+        context => q|Template variable.|
+    },
+
+    'price' => {
+        message => q|The unit price for this item.|,
+        lastUpdated => 0,
+        context => q|Template variable.|
+    },
+
+    'url' => {
+        message => q|A URL for viewing more detailed information about this item, from the Cart's viewItem screen.|,
+        lastUpdated => 0,
+        context => q|Template variable.|
+    },
+
+    'totalPrice' => {
+        message => q|The total price of all items in the cart, formatted to two decimal places.|,
+        lastUpdated => 0,
+        context => q|Template variable.|
+    },
+
+    'totalItems' => {
+        message => q|The total number of items in the cart.|,
+        lastUpdated => 0,
+        context => q|Template variable.|
+    },
+
+};
+
+1;
