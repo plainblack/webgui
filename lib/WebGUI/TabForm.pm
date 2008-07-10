02635bf3c98425ce22268512e15303b9258a87f5
Colin Kuskie <colink@perldreamer.com>
2008-07-10 20:32:14 +0000

    Add a logged warning for trying to fetch a non-existant tab by key.

--- lib/WebGUI/TabForm.pm
+++ lib/WebGUI/TabForm.pm
@@ -134,6 +134,7 @@ The name of the tab to return the form object for.
 sub getTab {
 	my $self = shift;
 	my $key = shift;
+    $self->session->log->warn('Trying to fetch non-existant tab: '.$key) if !exists $self->{_tab}{$key};
 	return $self->{_tab}{$key}{form};
 }
 
