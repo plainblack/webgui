d18f081683dc2189c24388c535ce07b8687e930e
JT Smith <jt@plainblack.com>
2008-09-15 15:08:40 +0000

    Made the charset metatag the highest thing in the head block.

--- lib/WebGUI/Session/Style.pm
+++ lib/WebGUI/Session/Style.pm
@@ -168,8 +168,8 @@ sub process {
 		$templateId = 'PBtmpl0000000000000132';
 	}
 $var{'head.tags'} = '
-<meta name="generator" content="WebGUI '.$WebGUI::VERSION.'" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="generator" content="WebGUI '.$WebGUI::VERSION.'" />
 <meta http-equiv="Content-Script-Type" content="text/javascript" />
 <meta http-equiv="Content-Style-Type" content="text/css" />
 <script type="text/javascript">
