47f62faeeefe025cb4adc0f4656a79fadfd46d0a
Colin Kuskie <colink@perldreamer.com>
2008-11-20 19:56:52 +0000

    merge in POD fixes from 7.5 branch

--- lib/WebGUI/Auth.pm
+++ lib/WebGUI/Auth.pm
@@ -626,7 +626,7 @@ sub getAccountTemplateId {
 
 #-------------------------------------------------------------------
 
-=head2 getAccountTemplateId ( )
+=head2 getCreateAccountTemplateId ( )
 
 This method should be overridden by the subclass and should return the template ID for the create account screen.
 
