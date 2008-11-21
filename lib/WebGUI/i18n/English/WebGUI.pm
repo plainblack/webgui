c4255305c0bb7e3af8f8e4e26b824b3671d9a6c7
Colin Kuskie <colink@perldreamer.com>
2008-11-21 18:18:00 +0000

    fix encrypt login hover help

--- lib/WebGUI/i18n/English/WebGUI.pm
+++ lib/WebGUI/i18n/English/WebGUI.pm
@@ -3244,10 +3244,9 @@ and tracked by WebGUI.|,
         },
 
         '1006 description' => {
-                message => q|Should the system use the HTTPS protocol for the login form?  Note that setting this option to true will encrypt not only the login form, but all pages after that as well.|,
-                lastUpdated => 1163457062,
+                message => q|Should the system use the HTTPS protocol for the login form?|,
+                lastUpdated => 1227291454,
         },
-
 
         '164 description' => {
                 message => q|<p>Set the default authentication method for new accounts.  The two available options by default are WebGUI and LDAP. WebGUI authentication means that the users will authenticate against the username and password stored in the WebGUI database. LDAP authentication means that users will authenticate against an external LDAP server.  Other methods can be provided by writing a custom authentication plug-in.</p>
