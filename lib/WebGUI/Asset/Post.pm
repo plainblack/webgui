80fdb029b5b0795a4932d5e57e83895bcb39ec00
JT Smith <jt@plainblack.com>
2008-12-16 23:01:47 +0000

    skip notify of spam

--- lib/WebGUI/Asset/Post.pm
+++ lib/WebGUI/Asset/Post.pm
@@ -959,6 +959,7 @@ sub postProcess {
     if (ref $spamStopWords eq 'ARRAY') {
         my $spamRegex = join('|',@{$spamStopWords});
         if ($data{content} =~ m/$spamRegex/xmsi) {
+            $data{skipNotification} = 1;
             $self->trash;
         }
     }
