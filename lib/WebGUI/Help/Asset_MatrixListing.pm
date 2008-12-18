d46193b07540a4db1635ab054b8576e1ef7f0bbe
Yung Han Khoe <yung@webgui-svn>
2008-12-18 11:25:33 +0000

    updating matrix help and international

---
+++ lib/WebGUI/Help/Asset_MatrixListing.pm
@@ -0,0 +1,93 @@
+package WebGUI::Help::Asset_MatrixListing;
+use strict;
+
+our $HELP = {
+    'listing detail template' => {
+        title     => 'detail template help title',
+        body      => '',
+        isa    => [
+            {   namespace => "Asset_Template",
+                tag       => "template variables",
+            },
+            {   tag       => 'asset template asset variables',
+                namespace => 'Asset'
+            },
+        ],
+        variables => [
+            { 'name' => 'screenshots' },
+            { 'name' => 'emailForm' },
+            { 'name' => 'emailSent' },
+            { 'name' => 'lastUpdated_epoch' },
+            { 'name' => 'lastUpdated_date' },
+            { 'name' => 'description' },
+            { 'name' => 'productName' },
+            { 'name' => 'productUrl' },
+            { 'name' => 'productUrl_click' },
+            { 'name' => 'manufacturerName' },
+            { 'name' => 'manufacturerUrl' },
+            { 'name' => 'manufacturerUrl_click' },
+            { 'name' => 'version' },
+            { 'name' => 'views' },
+            { 'name' => 'compares' },
+            { 'name' => 'clicks' },
+            { 'name' => 'ratings' },
+            {   'name'      => 'CATEGORY_NAME_loop',
+                'variables' => [
+                    { 'name' => 'categoryLabel' },
+                    {   'name'        => 'attribute_loop',
+                        'variables' => [
+                            { 'name' => 'label' },
+                            { 'name' => 'value' },
+                            { 'name' => 'fieldType' },
+                        ]
+                    }
+                ]
+            }
+        ],
+        related => [
+            {   tag       => 'search template',
+                namespace => 'Asset_Matrix'
+            },
+            {   tag       => 'compare template',
+                namespace => 'Asset_Matrix'
+            },
+            {   tag       => 'main template',
+                namespace => 'Asset_Matrix'
+            },
+            {   tag       => 'edit listing template',
+                namespace => 'Asset_MatrixListing'
+            },
+        ],
+    },
+
+    'edit listing template' => {
+        title     => 'edit listing template help title',
+        body      => '',
+        isa    => [
+            {   namespace => "Asset_Template",
+                tag       => "template variables",
+            },
+            {   tag       => 'asset template asset variables',
+                namespace => 'Asset'
+            },
+        ],
+        variables => [
+            {   'name'      => 'form',  }
+        ],
+        related => [
+            {   tag       => 'search template',
+                namespace => 'Asset_Matrix'
+            },
+            {   tag       => 'compare template',
+                namespace => 'Asset_Matrix'
+            },
+            {   tag       => 'main template',
+                namespace => 'Asset_Matrix'
+            },
+            {   tag       => 'listing detail template',
+                namespace => 'Asset_MatrixListing'
+            },
+        ],
+    },
+};
+
--- lib/WebGUI/i18n/English/Asset_MatrixListing.pm
+++ lib/WebGUI/i18n/English/Asset_MatrixListing.pm
@@ -13,7 +13,6 @@ our $I18N = {
         lastUpdated => 0,
     },
 
-
     'template description' => {
         message => q|Select a template to be used to display the default view of the Matrix.|,
         lastUpdated => 0,
@@ -194,7 +193,6 @@ it would be best to make sure the names are the same.|,
         lastUpdated => 0,
     },
 
-
     'show ratings' => {
         message => q|Show Ratings|,
         lastUpdated => 0,
@@ -205,6 +203,168 @@ it would be best to make sure the names are the same.|,
         lastUpdated => 0,
     },
 
+    'detail template help title' => {
+        lastUpdated => 0,
+        message => q|Matrix Listing Detail Template Variables|,
+	    context => q|The title of the help page for the matrix listing detail template.|,
+    },
+
+    'edit listing template help title' => {
+        lastUpdated => 0,
+        message => q|Edit Matrix Listing Template Variables|,
+    	context => q|The title of the help page for the edit listing template.|,
+    },
+
+    'form' => {
+        message => q|The complete edit form.|,
+        lastUpdated => 0,
+    	context => q|Description of the form tmpl_var for the template help.|,
+    },
+
+    'screenshots' => {
+        message => q|This listings screenshots displayed using the ukplayer.|,
+        lastUpdated => 0,
+	    context => q|Description of the screenshots tmpl_var for the template help.|,
+    },
+
+    'comments' => {
+        message => q|The comments attached to this listing.|,
+    	lastUpdated => 0,
+	    context => q|Description of the comments tmpl_var for the template help.|,
+    },
+
+    'emailForm' => {
+    	message => q|A form to use to send an email to the listing maintainer.|,
+	    lastUpdated => 0,
+    	context => q|Description of the emailForm tmpl_var for the template help.|,
+    },
+
+    'emailSent' => {
+        message => q|A condition indicating whether or not an email message was sent.|,
+	    lastUpdated => 0,
+    	context => q|Description of the emailSent tmpl_var for the template help.|,
+    },
+
+    'isPending' => {
+        message => q|A conditional indicating whether or not this listing has been approved or not.|,
+        lastUpdated => 1167186037,
+        context => q|Description of the CATEGORY_NAME_loop tmpl_var for the template help.|,
+    },
+
+    'lastUpdated_epoch' => {
+        message => q|The epoch date of when this listing was last updated.|,
+        lastUpdated => 0,
+        context => q|Description of the CATEGORY_NAME_loop tmpl_var for the template help.|,
+    },
+
+    'lastUpdated_date' => {
+        message => q|A human readable date of when this listing was last updated.|,
+        lastUpdated => 0,
+        context => q|Description of the CATEGORY_NAME_loop tmpl_var for the template help.|,
+    },
+
+    'description' => {
+	message => q|The description of this listing.|,
+	lastUpdated => 0,
+        context => q|Description of the description tmpl_var for the template help.|,
+    },
+
+    'productName' => {
+	message => q|The name of this listing. This is the same as this matrix listing's asset title.|,
+	lastUpdated => 0,
+        context => q|Description of the productName tmpl_var for the template help.|,
+    },
+
+    'productUrl' => {
+	message => q|The manufacturer's URL for this listing.|,
+	lastUpdated => 0,
+        context => q|Description of the productUrl tmpl_var for the template help.|,
+    },
+
+    'productUrl_click' => {
+	message => q|The product URL to use if you want to register the click count for this listing.|,
+	lastUpdated => 0,
+        context => q|Description of the productUrl_click tmpl_var for the template help.|,
+    },
+
+    'manufacturerUrl' => {
+	message => q|The manufacturer's URL.|,
+	lastUpdated => 0,
+        context => q|Description of the manufacturerUrl tmpl_var for the template help.|,
+    },
+
+    'manufacturerUrl_click' => {
+	message => q|The manufacturer URL to use if you want to register the click count for this listing.|,
+	lastUpdated => 0,
+        context => q|Description of the manufacturerUrl_click tmpl_var for the template help.|,
+    },
+
+    'views' => {
+	message => q|The total number of views this listing has received.|,
+	lastUpdated => 0,
+        context => q|Description of the views tmpl_var for the template help.|,
+    },
+
+    'compares' => {
+	message => q|The total number of compares this listing has received.|,
+	lastUpdated => 0,
+        context => q|Description of the compares tmpl_var for the template help.|,
+    },
+
+    'clicks' => {
+	message => q|The total number of clicks this listing has received.|,
+	lastUpdated => 0,
+        context => q|Description of the clicks tmpl_var for the template help.|,
+    },
+
+
+    'version' => {
+	message => q|The version of this listing.|,
+	lastUpdated => 0,
+        context => q|Description of the version tmpl_var for the template help.|,
+    },
+
+    'ratings' => {
+	message => q|An overview of the ratings this listing has received and a form to rate this listing if the user hasn't rated it already.|,
+	lastUpdated => 0,
+        context => q|Description of the ratings tmpl_var for the template help.|,
+    },
+
+    'CATEGORY_NAME_loop' => {
+	message => q|A loop containting attributes and values for those attributes is created for each category in this matrix. The name of the loop is the category name with spaces replaced with hyphens and a _loop added to the end. So if you have a category called "Bells and Whistles" then the loop would be called "bells-and-whistles_loop".|,
+	lastUpdated => 0,
+	context => q|Description of the CATEGORY_NAME_loop tmpl_var for the template help.|,
+    },
+
+    'categoryLabel' => {
+	message => q|The label of this category.|,
+	lastUpdated => 0,
+	context => q|Description of the categoryLabel tmpl_var in the CATEGORY_NAME_loop for the template help.|,
+    },
+
+    'attribute_loop' => {
+        message => q|A loop containing matrix attributes.|,
+        lastUpdated => 0,
+        context => q|Description of the attribute_loop tmpl_var for the template help.|,
+    },
+
+    'value' => {
+	message => q|The value of this attribute.|,
+	lastUpdated => 0,
+	context => q|Description of the value tmpl_var in the attribute_loop for the template help.|,
+    },
+
+    'label' => {
+	message => q|The label of this attribute.|,
+	lastUpdated => 0,
+	context => q|Description of the label tmpl_var in the attribute_loop for the template help.|,
+    },
+
+    'fieldType' => {
+	message => q|The fieldType of this attribute.|,
+	lastUpdated => 0,
+	context => q|Description of the fieldType tmpl_var in the attribute_loop for the template help.|,
+    },
 };
 
 1;
