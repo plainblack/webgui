24da72362934c00786b52e863f315c4677521e3a
Graham Knop <graham@plainblack.com>
2009-02-20 23:47:30 +0000

    updating copyright year

--- t/lib/WebGUI/PseudoRequest.pm
+++ t/lib/WebGUI/PseudoRequest.pm
@@ -16,7 +16,7 @@ $mocker->fake_module(
 =head1 LEGAL
 
  -------------------------------------------------------------------
-  WebGUI is Copyright 2001-2005 Plain Black Corporation.
+  WebGUI is Copyright 2001-2009 Plain Black Corporation.
  -------------------------------------------------------------------
   Please read the legal notices (docs/legal.txt) and the license
   (docs/license.txt) that came with this distribution before using
