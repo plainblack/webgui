e1f77b502d4abb54b5a38bbd87ee3042456db960
Colin Kuskie <colink@perldreamer.com>
2009-02-26 15:26:04 -0800

    Message Center i18n.

---
+++ lib/WebGUI/i18n/English/Message_Center.pm
@@ -0,0 +1,34 @@
+package WebGUI::i18n::English::Message_Center;
+
+use strict;
+
+our $I18N = {
+
+	'receive inbox notifications' => {
+		message => q|Inbox notifications contain.|,
+		context => q|Allows a user to choose how they get notified about things in their Inbox.|,
+		lastUpdated => 1235685248,
+	},
+
+	'short notification' => {
+		message => q|Short notification|,
+		context => q|An abbreviated, short message that they have an email.|,
+		lastUpdated => 1235685248,
+	},
+
+	'full message' => {
+		message => q|Full content from message|,
+		context => q|The entire content of the Inbox message.|,
+		lastUpdated => 1235685248,
+	},
+
+	'receive inbox emails' => {
+		message => q|Receive inbox notification via SMS?|,
+		context => q|Allows a user to choose how they get notified about things in their Inbox.|,
+		lastUpdated => 1235685248,
+	},
+
+};
+
+1;
+#vim:ft=perl
