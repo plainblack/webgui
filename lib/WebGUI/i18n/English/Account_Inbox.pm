9bab9286151ff9070f12cd5db317959cee9d6901
Colin Kuskie <colink@perldreamer.com>
2009-06-11 19:04:08 +0000

    Fix broken i18n labels in Account/Inbox.

--- lib/WebGUI/i18n/English/Account_Inbox.pm
+++ lib/WebGUI/i18n/English/Account_Inbox.pm
@@ -470,6 +470,11 @@ our $I18N = {
 		lastUpdated => 1181409056,
 	},
 
+    'invalid email' => {
+		message => q|Invalid email address entered: %s.|,
+		lastUpdated => 1181409056,
+	},
+
 	'missing message' => {
 		message     => q|Your invitiation must have a message.|,
 		lastUpdated => 1181409432,
@@ -482,6 +487,11 @@ our $I18N = {
 
     'already a member' => {
 		message     => q|The email address you entered is already registered to <a href="%s">%s</a> on this site.|,
+		lastUpdated => 1227715272,
+	},
+
+    'currently invited' => {
+		message     => q|The email address you entered, %s, has an outstanding invitation on this site.|,
 		lastUpdated => 1227715272,
 	},
 
