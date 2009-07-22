69a07bb672a724a98052827c6c42e0e8083ee2cb
JT Smith <jt@plainblack.com>
2009-07-22 22:00:45 +0000

    added legal section

--- lib/WebGUI/Shop/PayDriver/Cash.pm
+++ lib/WebGUI/Shop/PayDriver/Cash.pm
@@ -1,5 +1,19 @@
 package WebGUI::Shop::PayDriver::Cash;
 
+=head1 LEGAL
+
+ -------------------------------------------------------------------
+  WebGUI is Copyright 2001-2009 Plain Black Corporation.
+ -------------------------------------------------------------------
+  Please read the legal notices (docs/legal.txt) and the license
+  (docs/license.txt) that came with this distribution before using
+  this software.
+ -------------------------------------------------------------------
+  http://www.plainblack.com                     info@plainblack.com
+ -------------------------------------------------------------------
+
+=cut
+
 use strict;
 
 use WebGUI::Shop::PayDriver;
--- lib/WebGUI/Shop/PayDriver/PayPal/PayPalStd.pm
+++ lib/WebGUI/Shop/PayDriver/PayPal/PayPalStd.pm
@@ -1,23 +1,15 @@
 package WebGUI::Shop::PayDriver::PayPal::PayPalStd;
 
 =head1 LEGAL
- -------------------------------------------------------------------
- PayPal Standard payment driver for WebGUI.
- Copyright (C) 2009 Plain Black Corporation. 
- -------------------------------------------------------------------
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along
- with this program; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ -------------------------------------------------------------------
+  WebGUI is Copyright 2001-2009 Plain Black Corporation.
+ -------------------------------------------------------------------
+  Please read the legal notices (docs/legal.txt) and the license
+  (docs/license.txt) that came with this distribution before using
+  this software.
+ -------------------------------------------------------------------
+  http://www.plainblack.com                     info@plainblack.com
  -------------------------------------------------------------------
 
 =cut
