1bbbd75f8d4525d21121bdd1b5c3ed0b1bd4d3d9
Colin Kuskie <colink@perldreamer.com>
2009-09-29 14:56:21 -0700

    drop another SQL Form table if the SQL form isn't used.  Fixes bug #11061

--- docs/upgrades/upgrade_7.7.20-7.7.21.pl
+++ docs/upgrades/upgrade_7.7.20-7.7.21.pl
@@ -34,6 +34,7 @@ my $session = start(); # this line required
 fixEmptyCalendarIcalFeeds( $session );
 fixEMSTemplates( $session );
 removeOldSubscriptionTables( $session );
+removeSQLFormTables( $session );
 
 finish($session); # this line required
 
@@ -47,6 +48,20 @@ finish($session); # this line required
 #    print "DONE!\n" unless $quiet;
 #}
 
+# Describe what our function does
+#----------------------------------------------------------------------------
+sub removeSQLFormTables {
+    my $session = shift;
+    print "\tRemoving leftover SQL Form tables if not used... " unless $quiet;
+    my $tablesUsed = $session->db->quickScalar("select count(*) from asset where className='WebGUI::Asset::Wobject::SQLForm'");
+    if (!$tablesUsed) {
+        $session->db->write('DROP TABLE IF EXISTS SQLForm_fieldOrder');
+        print "\n\t\tSQL Form not used, dropping table...";
+    }
+    print "Done.\n" unless $quiet;
+}
+
+# Describe what our function does
 #----------------------------------------------------------------------------
 sub removeOldSubscriptionTables {
     my $session = shift;
