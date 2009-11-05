15514cb97a54b1f17f4e6ef86f860f52281a3003
Colin Kuskie <colink@perldreamer.com>
2009-11-05 12:50:00 -0800

    Begin international USPS shipping driver.

---
+++ lib/WebGUI/i18n/English/ShipDriver_USPSInternational.pm
@@ -0,0 +1,15 @@
+package WebGUI::i18n::English::ShipDriver_USPS;
+
+use strict;
+
+our $I18N = {
+
+    'U.S. Postal Service, International' => {
+        message => q|U.S. Postal Service, International|,
+        lastUpdated => 1203569535,
+        context => q|Name of the shipping driver|,
+    }
+
+};
+
+1;
