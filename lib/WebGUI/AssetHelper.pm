dd8674c49b3ca6c8c952ae4a3d0b5c4392736c48
Colin Kuskie <colink@perldreamer.com>
2009-11-24 13:08:48 -0800

    Add README file, and base class for AssetHelpers.

---
+++ lib/WebGUI/AssetHelper.pm
@@ -0,0 +1,42 @@
+package WebGUI::AssetHelper;
+
+=head1 LEGAL
+
+ -------------------------------------------------------------------
+  WebGUI is Copyright 2001-2009 Plain Black Corporation.
+ -------------------------------------------------------------------
+  Please read the legal notices (docs/legal.txt) and the license
+  (docs/license.txt) that came with this distribution before using
+  this software.
+ -------------------------------------------------------------------
+  http://www.plainblack.com                     info@plainblack.com
+ -------------------------------------------------------------------
+
+=head1 NAME
+
+Package WebGUI::AssetHelper
+
+=head1 DESCRIPTION
+
+Base class for all Asset Helpers, which provide editing and administrative controls for Assets inside
+the Admin Console.
+
+=head1 METHODS
+
+These methods are available from this class:
+
+=cut
+
+#-------------------------------------------------------------------
+
+=head2 process ( )
+
+Default method called by the Asset Helper content handler.
+
+=cut
+
+sub process {
+
+}
+
+1;
