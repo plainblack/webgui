b6058e8b111b995f213a8e12223d1ffaab31055c
Colin Kuskie <colink@perldreamer.com>
2009-12-05 19:06:05 -0800

    Began POD for WebGUI::Definition::Asset

--- lib/WebGUI/Definition/Asset.pm
+++ lib/WebGUI/Definition/Asset.pm
@@ -25,6 +25,26 @@ no warnings qw(uninitialized);
 
 our $VERSION = '0.0.1';
 
+=head1 NAME
+
+Package WebGUI::Definition::Asset
+
+=head1 DESCRIPTION
+
+Moose-based meta class for all Asset definitions in WebGUI.
+
+=head1 SYNOPSIS
+
+A definition contains all the information needed to build an object.
+Information required to build forms are added as optional roles and
+sub metaclasses.  Database persistance is handled similarly.
+
+=head1 METHODS
+
+These methods are available from this class:
+
+=cut
+
 my ($import, $unimport, $init_meta) = Moose::Exporter->build_import_methods(
     install         => [ 'unimport' ],
     also            => 'WebGUI::Definition',
@@ -47,6 +67,20 @@ sub init_meta {
     $options{metaclass} = 'WebGUI::Definition::Meta::Asset';
     return Moose->init_meta(%options);
 }
+
+#-------------------------------------------------------------------
+
+=head2 property ( $name, %options )
+
+Extends WebGUI::Definition::property to copy the table attribute from the
+meta class into the options for each property.
+
+=head3 $name
+
+=head3 %options
+
+=cut
+
 
 sub property {
     my ($meta, $name, %options) = @_;
