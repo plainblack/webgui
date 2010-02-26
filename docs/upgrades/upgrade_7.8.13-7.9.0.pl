a8e7ec090b152f75633418ac89c8eff65ad94c30
Doug Bell <doug@plainblack.com>
2010-02-25 22:38:05 -0600

    preparing for 7.9.0 dev

---
+++ docs/upgrades/upgrade_7.8.13-7.9.0.pl
@@ -0,0 +1,130 @@
+#!/usr/bin/env perl
+
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2009 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+our ($webguiRoot);
+
+BEGIN {
+    $webguiRoot = "../..";
+    unshift (@INC, $webguiRoot."/lib");
+}
+
+use strict;
+use Getopt::Long;
+use WebGUI::Session;
+use WebGUI::Storage;
+use WebGUI::Asset;
+
+
+my $toVersion = "7.9.0";
+my $quiet;
+
+
+my $session = start();
+
+# upgrade functions go here
+
+finish($session);
+
+
+#----------------------------------------------------------------------------
+# Describe what our function does
+#sub exampleFunction {
+#    my $session = shift;
+#    print "\tWe're doing some stuff here that you should know about... " unless $quiet;
+#    # and here's our code
+#    print "DONE!\n" unless $quiet;
+#}
+
+
+# -------------- DO NOT EDIT BELOW THIS LINE --------------------------------
+
+#----------------------------------------------------------------------------
+# Add a package to the import node
+sub addPackage {
+    my $session     = shift;
+    my $file        = shift;
+
+    # Make a storage location for the package
+    my $storage     = WebGUI::Storage->createTemp( $session );
+    $storage->addFileFromFilesystem( $file );
+
+    # Import the package into the import node
+    my $package = eval { WebGUI::Asset->getImportNode($session)->importPackage( $storage, { overwriteLatest => 1 } ); };
+
+    if ($package eq 'corrupt') {
+        die "Corrupt package found in $file.  Stopping upgrade.\n";
+    }
+    if ($@ || !defined $package) {
+        die "Error during package import on $file: $@\nStopping upgrade\n.";
+    }
+
+    # Turn off the package flag, and set the default flag for templates added
+    my $assetIds = $package->getLineage( ['self','descendants'] );
+    for my $assetId ( @{ $assetIds } ) {
+        my $asset   = WebGUI::Asset->newByDynamicClass( $session, $assetId );
+        if ( !$asset ) {
+            print "Couldn't instantiate asset with ID '$assetId'. Please check package '$file' for corruption.\n";
+            next;
+        }
+        my $properties = { isPackage => 0 };
+        if ($asset->isa('WebGUI::Asset::Template')) {
+            $properties->{isDefault} = 1;
+        }
+        $asset->update( $properties );
+    }
+
+    return;
+}
+
+#-------------------------------------------------
+sub start {
+    my $configFile;
+    $|=1; #disable output buffering
+    GetOptions(
+        'configFile=s'=>\$configFile,
+        'quiet'=>\$quiet
+    );
+    my $session = WebGUI::Session->open($webguiRoot,$configFile);
+    $session->user({userId=>3});
+    my $versionTag = WebGUI::VersionTag->getWorking($session);
+    $versionTag->set({name=>"Upgrade to ".$toVersion});
+    return $session;
+}
+
+#-------------------------------------------------
+sub finish {
+    my $session = shift;
+    updateTemplates($session);
+    my $versionTag = WebGUI::VersionTag->getWorking($session);
+    $versionTag->commit;
+    $session->db->write("insert into webguiVersion values (".$session->db->quote($toVersion).",'upgrade',".time().")");
+    $session->close();
+}
+
+#-------------------------------------------------
+sub updateTemplates {
+    my $session = shift;
+    return undef unless (-d "packages-".$toVersion);
+    print "\tUpdating packages.\n" unless ($quiet);
+    opendir(DIR,"packages-".$toVersion);
+    my @files = readdir(DIR);
+    closedir(DIR);
+    my $newFolder = undef;
+    foreach my $file (@files) {
+        next unless ($file =~ /\.wgpkg$/);
+        # Fix the filename to include a path
+        $file       = "packages-" . $toVersion . "/" . $file;
+        addPackage( $session, $file );
+    }
+}
+
+#vim:ft=perl
--- lib/WebGUI.pm
+++ lib/WebGUI.pm
@@ -1,8 +1,8 @@
 package WebGUI;
 
 
-our $VERSION = '7.8.13';
-our $STATUS = 'stable';
+our $VERSION = '7.9.0';
+our $STATUS = 'beta';
 
 
 =head1 LEGAL
