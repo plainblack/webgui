bc71165a164ee725aba98d671cf9b4efec45d0c5
Colin Kuskie <colink@perldreamer.com>
2010-03-09 13:43:58 -0800

    Add T::C tests for Redirect, RichEdit,Shortcut,Sku, Template and Wobject.

---
+++ t/tests/Test/Asset/RichEdit.pm
@@ -0,0 +1,34 @@
+package Test::Asset::RichEdit;
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2009 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+use FindBin;
+use lib "$FindBin::Bin/lib";
+
+use base qw/Test::AssetBase/;
+use WebGUI::Test;
+
+use Test::More;
+use Test::Deep;
+use Test::Exception;
+
+sub class {
+     return qw/WebGUI::Asset::RichEdit/;
+}
+
+sub uiLevel {
+     return 5;
+}
+
+sub list_of_tables {
+     return [qw/assetData RichEdit/];
+}
+
+1;
