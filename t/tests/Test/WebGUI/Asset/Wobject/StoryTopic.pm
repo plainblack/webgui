85121c586c23d02cf7ea31f6aea57344b3f501b5
Colin Kuskie <colink@perldreamer.com>
2010-03-12 09:00:20 -0800

    Move Test::Asset* to Test::WebGUI::Asset*

---
+++ t/tests/Test/WebGUI/Asset/Wobject/StoryTopic.pm
@@ -0,0 +1,29 @@
+package Test::WebGUI::Asset::Wobject::StoryTopic;
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2009 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+use FindBin;
+use lib "$FindBin::Bin/lib";
+
+use base qw/Test::WebGUI::AssetBase/;
+
+use Test::WebGUI::More;
+use Test::WebGUI::Deep;
+use Test::WebGUI::Exception;
+
+sub class {
+     return qw/WebGUI::Asset::Wobject::StoryTopic/;
+}
+
+sub list_of_tables {
+     return [qw/assetData wobject StoryTopic assetAspectRssFeed/];
+}
+
+1;
