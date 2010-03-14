3a2f94f1b38225c7f028b9478d4748197a11a757
Colin Kuskie <colink@perldreamer.com>
2010-03-13 20:27:20 -0800

    Test for some Sku's.

---
+++ t/tests/Test/WebGUI/Asset/Sku/Donation.pm
@@ -0,0 +1,24 @@
+package Test::WebGUI::Asset::Sku::Donation;
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2009 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+
+use base qw/Test::WebGUI::Asset/;
+
+use Test::More;
+use Test::Deep;
+use Test::Exception;
+
+
+sub list_of_tables {
+     return [qw/assetData sku donation/];
+}
+
+1;
