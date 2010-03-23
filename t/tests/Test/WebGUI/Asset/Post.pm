7c8979c823a1bc4a908c3cb9e9f11142a843082c
Colin Kuskie <colink@perldreamer.com>
2010-03-23 16:15:33 -0700

    Tests for Post and Thread

---
+++ t/tests/Test/WebGUI/Asset/Post.pm
@@ -0,0 +1,28 @@
+package Test::WebGUI::Asset::Post;
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2009 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+
+use base qw/Test::WebGUI::Asset/;
+
+use Test::More;
+use Test::Deep;
+use Test::Exception;
+
+
+sub list_of_tables {
+     return [qw/assetData Post/];
+}
+
+sub parent_list {
+    return [ map { "WebGUI::Asset::$_"} qw/Wobject::Collaboration Post::Thread/ ];
+}
+
+1;
