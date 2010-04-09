a7598482c5b121bd565b83995a9157588239ef53
Doug Bell <doug@plainblack.com>
2010-04-09 17:57:23 -0500

    initial locationbar commit

--- www/extras/admin/admin.js
+++ www/extras/admin/admin.js
@@ -6,7 +6,7 @@ if ( typeof WebGUI == "undefined" ) {
 WebGUI.Admin = {};
 
 WebGUI.Admin.LocationBar = (function(){
-    
+
     // Public stuff
 
     return function (id) {
@@ -17,7 +17,18 @@ WebGUI.Admin.LocationBar = (function(){
         var _element    = document.getElementById( self.id );
 
         function _init () {
-            _element.appendChild( document.createTextNode( "Location Bar" ) );
+            var back    = new YAHOO.widget.Button( "backButton", {
+                label       : '<img src="' + getWebguiProperty("extrasURL") + 'icon/arrow_left.png" />'
+            } );
+            var forward = new YAHOO.widget.Button( "forwardButton", {
+                label       : '<img src="' + getWebguiProperty("extrasURL") + 'icon/arrow_right.png" />'
+            } );
+            var search  = new YAHOO.widget.Button( "searchButton", {
+                label       : '<img src="' + getWebguiProperty("extrasURL") + 'icon/magnifier.png" />'
+            } );
+            var home    = new YAHOO.widget.Button( "homeButton", {
+                label       : '<img src="' + getWebguiProperty("extrasURL") + 'icon/house.png" />'
+            } );
         }
 
         _init();
