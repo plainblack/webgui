c8414bee52d5e2325a512d9f4125b4e94ffc8a01
Colin Kuskie <colink@perldreamer.com>
2010-04-19 08:21:19 -0700

    Check for lack of existence of an asset before logging an error.  Fixes bug #11526

--- lib/WebGUI/AssetClipboard.pm
+++ lib/WebGUI/AssetClipboard.pm
@@ -98,9 +98,12 @@ sub duplicate {
     my $newAsset    
         = $parent->addChild( $self->get, undef, $self->get("revisionDate"), { skipAutoCommitWorkflows => $options->{skipAutoCommitWorkflows} } );
 
-    $self->session->log->error(
-        sprintf "Unable to add child %s (%s) to %s (%s)", $self->getTitle, $self->getId, $parent->getTitle, $parent->getId
-    );
+    if (! $newAsset) {
+        $self->session->log->error(
+            sprintf "Unable to add child %s (%s) to %s (%s)", $self->getTitle, $self->getId, $parent->getTitle, $parent->getId
+        );
+        return undef;
+    }
     # Duplicate metadata fields
     my $sth = $self->session->db->read(
         "select * from metaData_values where assetId = ?", 
