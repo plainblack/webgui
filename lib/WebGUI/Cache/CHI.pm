ecde77e2a87cd87ce975c991b49edbaad3558a98
Doug Bell <doug@plainblack.com>
2010-04-29 11:55:30 -0500

    add perldoc

--- lib/WebGUI/Cache/CHI.pm
+++ lib/WebGUI/Cache/CHI.pm
@@ -4,10 +4,39 @@ use strict;
 use base 'WebGUI::Cache';
 use CHI;
 
+=head1 NAME
+
+WebGUI::Cache::CHI - CHI cache driver
+
+=head1 DESCRIPTION
+
+This is a WebGUI Cache driver to the CHI cache interface. This allows WebGUI
+sites to use any CHI::Driver like FastMmap and Memcached
+
+=head1 METHODS
+
+=cut
+
+#----------------------------------------------------------------------------
+
+=head2 delete ( )
+
+Delete the current key
+
+=cut
+
 sub delete {
     my ( $self ) = @_;
     return $self->{_chi}->remove( $self->{_key} );
 }
+
+#----------------------------------------------------------------------------
+
+=head2 deleteChunk ( partialKey )
+
+Delete multiple keys from the cache
+
+=cut
 
 sub deleteChunk {
     my ( $self, $key ) = @_;
@@ -19,15 +48,40 @@ sub deleteChunk {
     }
 }
 
+#----------------------------------------------------------------------------
+
+=head2 flush ( )
+
+Delete the entire cache namespace
+
+=cut
+
 sub flush {
     my ( $self ) = @_;
     $self->{_chi}->purge;
 }
 
+#----------------------------------------------------------------------------
+
+=head2 get ( )
+
+Get the data in the current key
+
+=cut
+
 sub get {
     my ( $self ) = @_;
     return $self->{_chi}->get( $self->{_key} );
 }
+
+#----------------------------------------------------------------------------
+
+=head2 new ( session, key [, namespace] )
+
+Create a new WebGUI::Cache object with the given key. The namespace defaults
+to the current site's configuration file name
+
+=cut
 
 sub new { 
     my ( $class, $session, $key, $namespace ) = @_;
@@ -63,12 +117,29 @@ sub new {
     return bless { _session => $session, _key => $key, _chi => $chi }, $class;
 }
 
+#----------------------------------------------------------------------------
+
+=head2 set ( content [, ttl ] )
+
+Set the content to the current key. ttl is the number of seconds the cache 
+should live.
+
+=cut
+
 sub set {
     my ( $self, $content, $ttl ) = @_;
     $ttl ||= 60;
     $self->{_chi}->set( $self->{_key}, $content, $ttl );
     return;
 }
+
+#----------------------------------------------------------------------------
+
+=head2 stats ( )
+
+Get the size of the cache
+
+=cut
 
 sub stats {
     my ( $self ) = @_;
