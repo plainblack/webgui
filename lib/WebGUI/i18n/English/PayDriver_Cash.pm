a7a522cabcb4f96f468865529643789a8bd9262a
Colin Kuskie <colink@perldreamer.com>
2010-05-10 08:23:22 -0700

    Fix copy paste errors in the Cash i18n.  Fixes bug #11561.

--- lib/WebGUI/i18n/English/PayDriver_Cash.pm
+++ lib/WebGUI/i18n/English/PayDriver_Cash.pm
@@ -7,87 +7,6 @@ our $I18N = {
                 lastUpdated => 0,
                 context => q|Default Cash payment gateway label|
         },
-    'phone' => {
-                message => q|Telephone Number|,
-                lastUpdated => 0,
-                context => q|Form label in the checkout form of the iTransact module.|
-        },
-        'country' => {
-                message => q|Country|,
-                lastUpdated => 0,
-                context => q|Form label in the checkout form of the iTransact module.|
-        },
-	'firstName' => {
-		message => q|First name|,
-		lastUpdated => 0,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	'lastName' => {
-		message => q|Last name|,
-		lastUpdated => 0,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	'address' => {
-		message => q|Address|,
-		lastUpdated => 1101772170,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	'city' => {
-		message => q|City|,
-		lastUpdated => 1101772171,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	'state' => {
-		message => q|State|,
-		lastUpdated => 1101772173,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	'zipcode' => {
-		message => q|Zipcode|,
-		lastUpdated => 1101772174,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	'email' => {
-		message => q|Email|,
-		lastUpdated => 1101772176,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	'cardNumber' => {
-		message => q|Credit card number|,
-		lastUpdated => 1101772177,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	'expiration date' => {
-		message => q|Expiration date|,
-		lastUpdated => 1101772180,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	'cvv2' => {
-		message => q|Verification number (ie. CVV2)|,
-		lastUpdated => 1101772182,
-		context => q|Form label in the checkout form of the iTransact module.|
-	},
-	
-	'vendorId' => {
-		message => q|Username (Vendor ID)|,
-		lastUpdated => 0,
-		context => q|Form label in the configuration form of the iTransact module.|
-	},
-	'use cvv2' => {
-		message => q|Use CVV2|,
-		lastUpdated => 0,
-		context => q|Form label in the configuration form of the iTransact module.|
-	},
-	'emailMessage' => {
-		message => q|Email message|,
-		lastUpdated => 0,
-		context => q|Form label in the configuration form of the iTransact module.|
-	},
-	'password' => {
-		message => q|Password|,
-		lastUpdated => 0,
-		context => q|Form label in the configuration form of the iTransact module.|
-	},
 
 	'module name' => {
 		message => q|Cash|,
@@ -95,41 +14,6 @@ our $I18N = {
 		context => q|The displayed name of the payment module.|
 	},
 
-	'invalid firstName' => {
-		message => q|You have to enter a valid first name.|,
-		lastUpdated => 0,
-		context => q|An error indicating that an invalid first name has been entered.|
-	},
-	'invalid lastName' => {
-		message => q|You have to enter a valid last name.|,
-		lastUpdated => 0,
-		context => q|An error indicating that an invalid last name has been entered.|
-	},
-	'invalid address' => {
-		message => q|You have to enter a valid address.|,
-		lastUpdated => 0,
-		context => q|An error indicating that an invalid street has been entered.|
-	},
-	'invalid city' => {
-		message => q|You have to enter a valid city.|,
-		lastUpdated => 0,
-		context => q|An error indicating that an invalid city has been entered.|
-	},
-	'invalid zip' => {
-		message => q|You have to enter a valid zipcode.|,
-		lastUpdated => 0,
-		context => q|An error indicating that an invalid zipcode has been entered.|
-	},
-	'invalid email' => {
-		message => q|You have to enter a valid email address.|,
-		lastUpdated => 0,
-		context => q|An error indicating that an invalid email address has been entered.|
-	},
-	'no description' => {
-		message => q|No description|,
-		lastUpdated => 0,
-		context => q|The default description of purchase of users.|
-	},
 	'cash' => {
 		message => q|Cash|,
 		lastUpdated => 0,
