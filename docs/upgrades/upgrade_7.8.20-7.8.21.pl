723e37626b0b4f65a278b08b3a4083c2d0786b28
Colin Kuskie <colink@perldreamer.com>
2010-06-03 10:58:12 -0700

    Do not delete cron tasks with errors, just notify spectre of the problem.  Fixes bug #11604.

--- docs/upgrades/upgrade_7.8.20-7.8.21.pl
+++ docs/upgrades/upgrade_7.8.20-7.8.21.pl
@@ -22,6 +22,10 @@ use Getopt::Long;
 use WebGUI::Session;
 use WebGUI::Storage;
 use WebGUI::Asset;
+use WebGUI::Asset::Wobject::Collaboration;
+use WebGUI::Exception;
+use WebGUI::Workflow::Cron;
+use WebGUI::Utility qw/isIn/;
 
 
 my $toVersion = '7.8.21';
@@ -31,6 +35,8 @@ my $quiet; # this line required
 my $session = start(); # this line required
 
 # upgrade functions go here
+restoreDefaultCronJobs($session);
+restoreCsCronJobs($session);
 
 finish($session); # this line required
 
@@ -43,6 +49,101 @@ finish($session); # this line required
 #    # and here's our code
 #    print "DONE!\n" unless $quiet;
 #}
+
+#----------------------------------------------------------------------------
+# Describe what our function does
+sub restoreDefaultCronJobs {
+    my $session = shift;
+    # and here's our code
+    print "\tRestore missing default cron jobs that may have been deleted... " unless $quiet;
+    my $tasks = WebGUI::Workflow::Cron->getAllTasks($session);
+    my @taskIds = map { $_->getId } @{ $tasks };
+    if (! isIn('pbcron0000000000000001', @taskIds)) {
+        print "\n\t\tRestoring Daily Maintenance Task... " unless $quiet;
+        WebGUI::Workflow::Cron->create($session, {
+            title        => "Daily Maintenance", dayOfMonth   => '*',
+            enabled      => 1,                   monthOfYear  => '*',
+            runOnce      => 0,                   dayOfWeek    => '*',
+            minuteOfHour => 30,                  workflowId   => 'pbworkflow000000000001',
+            hourOfDay    => 23,                  priority     => 3,
+        },
+        'pbcron0000000000000001');
+    }
+    if (! isIn('pbcron0000000000000002', @taskIds)) {
+        print "\n\t\tRestoring Weekly Maintenance Task... " unless $quiet;
+        WebGUI::Workflow::Cron->create($session, {
+            title        => "Weekly Maintenance", dayOfMonth   => '*',
+            enabled      => 1,                    monthOfYear  => '*',
+            runOnce      => 0,                    dayOfWeek    => '0',
+            minuteOfHour => 30,                   workflowId   => 'pbworkflow000000000002',
+            hourOfDay    => 1,                    priority     => 3,
+        },
+        'pbcron0000000000000002');
+    }
+    if (! isIn('pbcron0000000000000003', @taskIds)) {
+        print "\n\t\tRestoring Hourly Maintenance Task... " unless $quiet;
+        WebGUI::Workflow::Cron->create($session, {
+            title        => "Hourly Maintenance", dayOfMonth   => '*',
+            enabled      => 1,                    monthOfYear  => '*',
+            runOnce      => 0,                    dayOfWeek    => '*',
+            minuteOfHour => 15,                   workflowId   => 'pbworkflow000000000004',
+            hourOfDay    => '*',                  priority     => 3,
+        },
+        'pbcron0000000000000003');
+    }
+    if (! isIn('pbcron0000000000000004', @taskIds)) {
+        print "\n\t\tRestoring Email Delivery Task... " unless $quiet;
+        WebGUI::Workflow::Cron->create($session, {
+            title        => "Send Queued Email Messages Every 5 Minutes",
+                                                  dayOfMonth   => '*',
+            enabled      => 1,                    monthOfYear  => '*',
+            runOnce      => 0,                    dayOfWeek    => '*',
+            minuteOfHour => '*/5',                workflowId   => 'pbworkflow000000000007',
+            hourOfDay    => '*',                  priority     => 3,
+        },
+        'pbcron0000000000000004');
+    }
+    print "DONE!\n" unless $quiet;
+}
+
+#----------------------------------------------------------------------------
+# Describe what our function does
+sub restoreCsCronJobs {
+    my $session = shift;
+    print "\tRestore missing Collaboration System cron jobs that may have been deleted... " unless $quiet;
+    my $i18n = WebGUI::International->new($session, "Asset_Collaboration");
+    my $getCs = WebGUI::Asset::Wobject::Collaboration->getIsa($session);
+    CS: while (1) {
+        my $cs = eval { $getCs->(); };
+        if (my $e = Exception::Class->caught()) {
+            $session->log->error($@);
+            next CS;
+        }
+        last CS unless $cs;
+        ##Do something useful with $product
+        my $cron = undef;
+        if ($cs->get("getMailCronId")) {
+            $cron = WebGUI::Workflow::Cron->new($session, $cs->get("getMailCronId"));
+        }
+        next CS if $cron;
+        $cron = WebGUI::Workflow::Cron->create($session, {
+                title        => $cs->getTitle." ".$i18n->get("mail"),
+                minuteOfHour => "*/".($cs->get("getMailInterval")/60),
+                className    => (ref $cs),
+                methodName   => "new",
+                parameters   => $cs->getId,
+                workflowId   => "csworkflow000000000001"
+                });
+        $cs->update({getMailCronId=>$cron->getId});
+        if ($cs->get("getMail")) {
+                $cron->set({enabled=>1,title=>$cs->getTitle." ".$i18n->get("mail"), minuteOfHour=>"*/".($cs->get("getMailInterval")/60)});
+        } else {
+                $cron->set({enabled=>0,title=>$cs->getTitle." ".$i18n->get("mail"), minuteOfHour=>"*/".($cs->get("getMailInterval")/60)});
+        }
+
+    }
+    print "DONE!\n" unless $quiet;
+}
 
 
 # -------------- DO NOT EDIT BELOW THIS LINE --------------------------------
