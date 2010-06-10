b74d21102c73514778624d095393a93a62eb3e6b
Colin Kuskie <colink@perldreamer.com>
2010-06-10 11:01:20 -0700

    Temporary config file fix.

--- t/lib/WebGUI/Test.pm
+++ t/lib/WebGUI/Test.pm
@@ -108,7 +108,7 @@ sub import {
             unless -f _;
         die "WEBGUI_CONFIG path '$config' is not readable by effective uid '$>'.\n"
             unless -r _;
-        $original_config_file = abs2rel( $config, catdir( $CLASS->root, 'etc') );
+        $original_config_file = $config;
     }
     goto &{ $_[0]->can('SUPER::import') };
 }
@@ -382,7 +382,8 @@ sub file {
         OPEN => 0,
         TMPDIR => 1,
     );
-    File::Copy::copy($original_config_file, $config_copy_abs);
+    File::Copy::copy($original_config_file, $config_copy_abs)
+        or die "Error creating temp config file: $!";
     $CLASS->addToCleanup(sub {
         unlink $config_copy_abs;
         undef $config_copy;
