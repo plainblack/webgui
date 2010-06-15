541f262b76866b07a604c3c0a05de5d5ca135726
Colin Kuskie <colink@perldreamer.com>
2010-06-15 11:03:55 -0700

    There's no need to special case Posts in Account/Contributions.  The view method referes you to the Thread, and if a user has multiple posts in a thread, it links to the Thread multiple times.  Also, this fixes bug #11622.

--- lib/WebGUI/Account/Contributions.pm
+++ lib/WebGUI/Account/Contributions.pm
@@ -200,12 +200,6 @@ sub www_view {
         my $asset      = WebGUI::Asset->newByDynamicClass( $session, $assetId );
         my $props      = $asset->get;
         $props->{url}  = $asset->getUrl;
-        if (ref $asset eq "WebGUI::Asset::Post") {
-            $asset = $asset->getThread;
-            $props = $asset->get;
-            $props->{className} = "WebGUI::Asset::Post";
-        }
-        
         push(@contribs,$props);
     }
     my $contribsCount  = $p->getRowCount;
