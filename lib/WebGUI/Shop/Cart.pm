c931cab85c7bf89977fa4d6a4e54336f96dbe5f1
Doug Bell <doug@plainblack.com>
2010-07-29 17:33:02 -0500

    fix #11755: New cart does not update shipping

--- lib/WebGUI/Shop/Cart.pm
+++ lib/WebGUI/Shop/Cart.pm
@@ -1252,9 +1252,8 @@ sub www_view {
 
     my $style = $session->style;
     my $yui = $url->extras('/yui/build');
-    $style->setScript("$yui/yahoo/yahoo-min.js");
+    $style->setScript("$yui/yahoo-dom-event/yahoo-dom-event.js");
     $style->setScript("$yui/json/json-min.js");
-    $style->setScript("$yui/event/event-min.js");
     $style->setScript("$yui/connection/connection-min.js");
     $style->setScript($url->extras('underscore/underscore-min.js'));
     $style->setScript($url->extras('shop/cart.js'), undef, 1);
