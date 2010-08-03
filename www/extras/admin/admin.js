ccb1f4dc621ce5343b1db822272652797e080a02
Doug Bell <doug@plainblack.com>
2010-08-03 13:54:12 -0500

    Safari doesn't like keywords being used as props

--- www/extras/admin/admin.js
+++ www/extras/admin/admin.js
@@ -366,7 +366,7 @@ WebGUI.Admin.prototype.addHelperHandler
     if ( helper.url ) {
         YAHOO.util.Event.on( elem, "click", function(){ self.gotoAsset( helper.url ) }, self, true );
     }
-    else if ( helper.class ) {
+    else if ( helper['class'] ) {
         YAHOO.util.Event.on( elem, "click", function(){ self.requestHelper( helper.class, self.currentAssetDef.assetId ) }, self, true );
     }
 };
