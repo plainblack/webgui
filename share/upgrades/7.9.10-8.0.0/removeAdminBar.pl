f1fed19e24a83ddf3a7a63490ba065c05d8b8f8d
Doug Bell <doug@plainblack.com>
2010-08-06 16:02:23 -0500

    remove AdminBar macro from all templates

--- share/upgrades/7.9.10-8.0.0/removeAdminBar.pl
+++ share/upgrades/7.9.10-8.0.0/removeAdminBar.pl
@@ -1,10 +1,36 @@
 
 use WebGUI::Upgrade::Script;
 
-
 report "\tRemoving Admin Bar... ";
 
 session->config->delete( 'macros/AdminBar' );
+
+report "\tEditing templates to remove AdminBar macro calls...";
+
+use WebGUI::Macro;
+use WebGUI::Asset::Template;
+
+my $iter    = WebGUI::Asset::Template->getIsa( session );
+ASSET: while (1) {
+    my $template = eval { $iter->() };
+    if (my $e = Exception::Class->caught()) {
+        session->log->error($@);
+        next ASSET;
+    }
+    last ASSET unless $template;
+
+    my $content = $template->template;
+    while ( $content =~ m/$WebGUI::Macro::macro_re/g ) {
+        my $macroCall   = $1;
+        my $macroName   = $2;
+        if ( $macroName eq 'AdminBar' ) {
+            $content    =~ s/\Q$macroCall//g;
+        }
+    }
+
+    $template->template( $content );
+    $template->write;
+}
 
 
 done;
