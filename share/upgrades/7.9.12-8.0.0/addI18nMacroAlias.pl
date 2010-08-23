d087b771315594bc8b9d656705cd0e5333225caf
Graham Knop <haarg@haarg.org>
2010-08-23 11:58:52 -0500

    clean up some upgrade scripts

--- share/upgrades/7.9.12-8.0.0/addI18nMacroAlias.pl
+++ share/upgrades/7.9.12-8.0.0/addI18nMacroAlias.pl
@@ -1,7 +1,7 @@
 
 use WebGUI::Upgrade::Script;
 
-report "\tAdding International macro alias: ^i18n(...);";
+start_step "Adding International macro alias: ^i18n(...);";
 
 session->config->addToHash( 'macros', 'i18n' => 'International' );
 
--- share/upgrades/7.9.12-8.0.0/addNewAdminConsole.pl
+++ share/upgrades/7.9.12-8.0.0/addNewAdminConsole.pl
@@ -2,13 +2,11 @@
 use WebGUI::Upgrade::Script;
 use List::MoreUtils qw( any );
 
-report "\tAdding new Admin Console...";
+start_step "Adding new Admin Console";
 
-if ( ! any { $_ eq 'WebGUI::Content::Admin' } @{session->config->get('contentHandlers')} ) {
-    session->config->addToArrayAfter( 
-        'contentHandlers', 'WebGUI::Content::Referral', 'WebGUI::Content::Admin'
-    );
-}
+session->config->addToArrayAfter(
+    'contentHandlers', 'WebGUI::Content::Referral', 'WebGUI::Content::Admin'
+);
 
 # Remove irrelevant Admin Console items
 session->config->deleteFromHash( 'adminConsole', 'adminConsoleOff' );
