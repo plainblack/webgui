200970cfd9d8c6c2d269f6ba58bebb9a5e6c8c3a
Colin Kuskie <colink@perldreamer.com>
2010-09-20 17:22:34 -0700

    Tests for List, Radio, Checkbox, SelectBox, SelectList, Username

---
+++ t/tests/Test/WebGUI/Form/List.pm
@@ -0,0 +1,19 @@
+package Test::WebGUI::Form::List;
+#-------------------------------------------------------------------
+# WebGUI is Copyright 2001-2009 Plain Black Corporation.
+#-------------------------------------------------------------------
+# Please read the legal notices (docs/legal.txt) and the license
+# (docs/license.txt) that came with this distribution before using
+# this software.
+#-------------------------------------------------------------------
+# http://www.plainblack.com                     info@plainblack.com
+#-------------------------------------------------------------------
+
+use strict;
+use warnings;
+
+use Test::More;
+
+use base qw/Test::WebGUI::Form::Control/;
+
+1;
