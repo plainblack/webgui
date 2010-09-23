a3d4f0fcc0b1b334129bf0f2e67f6386ea653019
Colin Kuskie <colink@perldreamer.com>
2010-09-23 09:04:29 -0700

    Add basic POD for WebGUI's debug panel plugin

--- lib/Plack/Middleware/Debug/Logger.pm
+++ lib/Plack/Middleware/Debug/Logger.pm
@@ -1,9 +1,51 @@
 package Plack::Middleware::Debug::Logger;
+
+=head1 LEGAL
+
+ -------------------------------------------------------------------
+  WebGUI is Copyright 2001-2009 Plain Black Corporation.
+ -------------------------------------------------------------------
+  Please read the legal notices (docs/legal.txt) and the license
+  (docs/license.txt) that came with this distribution before using
+  this software.
+ -------------------------------------------------------------------
+  http://www.plainblack.com                     info@plainblack.com
+ -------------------------------------------------------------------
+
+=head1 NAME
+
+Package Plack::Middleware::Debug::Logger
+
+=head1 DESCRIPTION
+
+This package is the interface to the WebGUI macro system.
+
+=cut
+
 use 5.008;
 use strict;
 use warnings;
 use parent qw(Plack::Middleware::Debug::Base);
 our $VERSION = '0.07';
+
+=head2 run
+
+Entry subroutine for the Debug logger.  Sets up $env->{'psgix.logger'} with a subref for
+logging information to the Debug panel.
+
+=head3 $env->{'psgix.logger'} 
+
+The subroutine takes a hash of arguments:
+
+=head4 level
+
+The severity level of the message.
+
+=head4 message
+
+The message to log.
+
+=cut
 
 sub run {
     my ($self, $env, $panel) = @_;
