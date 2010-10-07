af190441d320e5b805183a4044439585e02a7a54
Colin Kuskie <colink@perldreamer.com>
2010-10-07 08:59:17 -0700

    Add a dependency in testEnvironment.pl for Test::Differences.  Thanks to C.J. Adams-Collier.

--- sbin/testEnvironment.pl
+++ sbin/testEnvironment.pl
@@ -68,6 +68,7 @@ checkModule("Test::More",                   0.82,      2 );
 checkModule("Test::MockObject",             1.02,      2 );
 checkModule("Test::Deep",                   0.095,       );
 checkModule("Test::Exception",              0.27,      2 );
+checkModule("Test::Differences",            0.5,       2 );
 checkModule("Test::Class",                  0.31,      2 );
 checkModule("Pod::Coverage",                0.19,      2 );
 checkModule("Text::Balanced",               2.00,      2 );
