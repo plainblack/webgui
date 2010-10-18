3371c2b6a0a1ffd4f36e7439bf7600895185a337
Colin Kuskie <colink@perldreamer.com>
2010-10-18 13:53:33 -0700

    Template the emails sent out during content approval.  Includes new template, i18n, Help docs upgrade sub and code.  Fixes bug #11901.

--- lib/WebGUI/i18n/English/Workflow_Activity_NotifyAboutVersionTag.pm
+++ lib/WebGUI/i18n/English/Workflow_Activity_NotifyAboutVersionTag.pm
@@ -2,11 +2,48 @@ package WebGUI::i18n::English::Workflow_Activity_NotifyAboutVersionTag;
 use strict;
 
 our $I18N = {
-	'activityName' => {
-		message => q|Notify About Version Tag|,
-		context => q|The name of this workflow activity.|,
-		lastUpdated => 0,
-	},
+
+    'activityName' => {
+        message => q|Notify About Version Tag|,
+        context => q|The name of this workflow activity.|,
+        lastUpdated => 0,
+    },
+
+    'email template' => {
+        message => q|Email Template|,
+        context => q|Label in the edit form.  A template for an email.|,
+        lastUpdated => 0,
+    },
+
+    'email template help' => {
+        message => q|Select a template for the email that is sent out.|,
+        context => q|Hoverhelp.|,
+        lastUpdated => 0,
+    },
+
+    'email template title' => {
+        message => q|Notify About Version Tag Email Template|,
+        context => q|Title of the template variable page.|,
+        lastUpdated => 0,
+    },
+
+    'message' => {
+        message => q|The message from the Workflow Activity settings|,
+        context => q|Title of the template variable page.|,
+        lastUpdated => 0,
+    },
+
+    'comments' => {
+        message => q|Comments from the version tag, when it was committed.  These could be blank if no comments were entered, or if comments are not required on the site.|,
+        context => q|Title of the template variable page.|,
+        lastUpdated => 0,
+    },
+
+    'url' => {
+        message => q|The complete URL to work on the version tag.  It is a bare URL, with no HTML wrapped around it.|,
+        context => q|Title of the template variable page.|,
+        lastUpdated => 0,
+    },
 
 };
 
