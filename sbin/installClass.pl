e222cd97b63164a36fdee07e8e48ad95763fd7cc
Graham Knop <haarg@haarg.org>
2010-12-13 19:01:38 -0600

    move module loading to correct location

--- sbin/installClass.pl
+++ sbin/installClass.pl
@@ -15,16 +15,16 @@ use strict;
 use File::Basename ();
 use File::Spec;
 
-use Getopt::Long;
-use Pod::Usage;
-use WebGUI::Pluggable;
-use WebGUI::Session;
-
 my $webguiRoot;
 BEGIN {
     $webguiRoot = File::Spec->rel2abs(File::Spec->catdir(File::Basename::dirname(__FILE__), File::Spec->updir));
     unshift @INC, File::Spec->catdir($webguiRoot, 'lib');
 }
+
+use Getopt::Long;
+use Pod::Usage;
+use WebGUI::Pluggable;
+use WebGUI::Session;
 
 $|++;
 
