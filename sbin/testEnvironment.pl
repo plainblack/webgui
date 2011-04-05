3466181a05aa192bc9b6b2e4a0833528dc98a8eb
Colin Kuskie <colink@perldreamer.com>
2011-04-05 11:05:17 -0700

    Make an explicit dependency on HTML::Form for testing only.

--- sbin/testEnvironment.pl
+++ sbin/testEnvironment.pl
@@ -62,6 +62,7 @@ if ($] >= 5.008) {
 my $missingModule = 0;
 
 checkModule("LWP",                          5.833        );
+checkModule("HTML::Form",                   5.800,     2 );
 checkModule("Net::DNS",                     0.66,        );
 checkModule("HTTP::Request",                1.40         );
 checkModule("HTTP::Headers",                1.61         );
