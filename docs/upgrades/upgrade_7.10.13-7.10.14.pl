0bcdd9cc86f6afc7cbbb93d786949a2ed175ce2f
Colin Kuskie <colink@perldreamer.com>
2011-04-07 10:25:34 -0700

    Remove copies of templates that were added as part of the 7.10.13 upgrade.  Fix the upgrade package, too, in case people stop on .13.

--- docs/upgrades/upgrade_7.10.13-7.10.14.pl
+++ docs/upgrades/upgrade_7.10.13-7.10.14.pl
@@ -32,6 +32,7 @@ my $session = start(); # this line required
 
 # upgrade functions go here
 addOrganizationsToTransaction($session);
+removeDuplicateUndergroundStyleTemplates($session);
 
 finish($session); # this line required
 
@@ -55,6 +56,20 @@ sub addOrganizationsToTransaction {
     $session->db->write('ALTER TABLE transaction     ADD COLUMN shippingOrganization CHAR(35)');
     $session->db->write('ALTER TABLE transaction     ADD COLUMN paymentOrganization CHAR(35)');
     $session->db->write('ALTER TABLE transactionItem ADD COLUMN shippingOrganization CHAR(35)');
+    print "DONE!\n" unless $quiet;
+}
+
+#----------------------------------------------------------------------------
+# Describe what our function does
+sub removeDuplicateUndergroundStyleTemplates {
+    my $session = shift;
+    print "\tRemove duplicate Underground Style templatess that were mistakenly added during the 7.10.13 upgrade... " unless $quiet;
+    # and here's our code
+    ASSETID: foreach my $assetId(qw/IeFioyemW2Ov-hFGFwD75A niYg8Da1sULTQnevZ8wYpw/) {
+        my $asset = WebGUI::Asset->newByDynamicClass($session, $assetId);
+        next ASSETID unless $asset;
+        $asset->purge;  ##Kill it, crush it, grind its bits into dust.
+    }
     print "DONE!\n" unless $quiet;
 }
 
