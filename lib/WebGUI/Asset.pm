a4624663cf7f06614a2e288d8d50f74e34e0af20
Scott Walters <scott@slowass.net>
2011-09-06 20:26:55 -0400

    Mobile template is not being inherited  (#12246) Applied SquOnk's patch and added tests; modify www_add to inherit mobileStyleTemplateId from the parent as well as mobileTemplateId.  mobileTemplateId is only used by PageLayout currently.  If other assets start using this and this gets inherited cross asset types, thing will break.  A better design here would be to flag on a case-by-case basis in asset definitions whether any given property should be inherited globally, of children of the same asset class, or not at all.

--- lib/WebGUI/Asset.pm
+++ lib/WebGUI/Asset.pm
@@ -2921,6 +2921,8 @@ sub www_add {
 		encryptPage => $self->get("encryptPage"),
 		styleTemplateId => $self->get("styleTemplateId"),
 		printableStyleTemplateId => $self->get("printableStyleTemplateId"),
+		mobileStyleTemplateId => $self->get("mobileStyleTemplateId"),
+		mobileTemplateId => $self->get("mobileTemplateId"),
 		isHidden => $self->get("isHidden"),
 		className=>$class,
 		assetId=>"new",
