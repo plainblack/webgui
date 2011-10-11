f479d2ece85753018056e20977b4b81993e320b5
Colin Kuskie <colink@perldreamer.com>
2011-10-11 15:59:14 -0700

    Remove the FilePile asset from the file system and the config files.

---
+++ share/upgrades/7.10.18-8.0.0/removeFilePile.pl
@@ -0,0 +1,17 @@
+use WebGUI::Upgrade::Script;
+use File::Spec;
+use WebGUI::Paths;
+use Cwd qw(realpath);
+
+start_step "Removing FilePile asset from config file";
+
+session->config->delete( 'assets/FilePile' );
+
+done;
+
+start_step "Removing FilePile asset module";
+
+my $webgui_root = realpath( File::Spec->catdir( WebGUI::Paths->configBase, (File::Spec->updir) x 1 ) );
+unlink File::Spec->catfile($webgui_root, 'lib', 'WebGUI', 'Asset', 'FilePile');
+
+done;
