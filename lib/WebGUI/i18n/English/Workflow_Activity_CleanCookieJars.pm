ecc9a607e996f3d73948732c8576828c4d641db3
David Delikat <david-delikat@usa.net>
2012-03-20 20:02:53 +0000

    cleanup cookie jars workflow

---
+++ lib/WebGUI/i18n/English/Workflow_Activity_CleanCookieJars.pm
@@ -0,0 +1,19 @@
+package WebGUI::i18n::English::Workflow_Activity_CleanCookieJars;
+use strict;
+
+our $I18N = {
+	'activity cleanup cookie jars' => {
+		message => q|Clean Stale Cookie-Jar Files|,
+		context => q|The name of this workflow activity.|,
+		lastUpdated => 0,
+	},
+
+#	'activityName' => {
+#		message => q|Clean Temp Storage|,
+#		context => q|The name of this workflow activity.|,
+#		lastUpdated => 0,
+#	},
+
+};
+
+1;
